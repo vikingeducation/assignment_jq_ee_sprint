6dbe66c2cf8e0865db5275acb28a56965e6a7eef
Michael Alexander <alexandermw@gmail.com>
2014-11-13 14:31:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 $(document).ready({
   
   $('input[type="text"]').on('keypress', function(e){
-    $target = $(e.target)
-    textLength = ($target.value).length
+    var $target = $(e.target);
+    var textLength = ($target.value).length;
+    var max = 32
+    $validationDiv = $('<span class = "validation">' + textLength +' characters remaining(out of ' + max + ')</span>');
     
-    
+    if ($target.next().filter('.validation').length && textLength) {
+      $target.next().replaceWith($validationDiv);
+    } else if (textLength) {
+      $target.after($validationDiv);
+    } else if ($target.next().filter('.validation').length) {
+      $target.next().remove();
+    };
     
   
     
