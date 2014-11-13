26ce82ac5b9657d9fcec2edfde42a4230b6d36ed
Michael Alexander <alexandermw@gmail.com>
2014-11-13 12:52:27 -0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready({
+  
+  $('input[type="text"]').on('keypress', function(e){
+    $target = $(e.target)
+    textLength = ($target.value).length
+    
+    
+    
+  
+    
+    
+  });
+  
+  var charRemainingSpan = function(length, max){
+    
+    
+  };
+  
+});
