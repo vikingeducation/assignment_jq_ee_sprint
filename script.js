10b16e324c36fdcee31f2c8baa6998f8812b8fbc
Andur Carr <carr.andur@gmail.com>
2014-11-13 20:02:13 -0800

    Add form feedback

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
 $( document ).ready(function() {
 
+  var $fields    = $('.meaningless-input');
+  var $pass      = $('input[type="password"]').first();
+  var $confirm   = $('input[type="password"]').last();
+  var $form      = $('form');
+  var errorCount;
+
   var counter = function(field, max){
     var comparison = max - field.val().length;
     if (comparison == max) { return ""; }
     else { return comparison; }
   };
 
-  $('input[type="text"]').keyup(function(e) {
-    $("#text-counter").text(counter($(e.target), 32));
+  var confirmation = function(pw, confirm){
+    if (confirm.length === 0) { return ""; }
+    else if(pw == confirm) { return "Confirmed"; }
+    else { return "Passwords do not match"; }
+  };
+
+  $fields.keyup(function(e){
+    var $field = $(e.target);
+    $field.next().text(counter($field,
+      $field.data('max')
+    ));
   });
 
-  $('textarea').keyup(function(e) {
-    $("#textarea-counter").text(counter($(e.target), 140));
+  $confirm.keyup(function(e) {
+    $("#confirm-password").text(confirmation($pass.val(), $confirm.val()));
   });
 
-  $('input[type="password"]#pw').keyup(function(e) {
-    $("#password-counter").text(counter($(e.target), 16));
-  });
+  var validateLength = function(field) {
+    if (field.val().length > field.data('max'))
+      { field.addClass("error-field");
+      field.next().text("Too long"); }
+    else if (field.val().length < field.data('min'))
+      { field.addClass("error-field");
+      field.next().text("Too short"); }
+    else
+      { field.removeClass("error-field"); }
+  };
 
-  $('input[type="password"]#confirm').keyup(function(e) {
-    $("#confirm-counter").text(counter($(e.target), 16));
+  $form.submit(function(e){
+    e.preventDefault();
+    $fields.each(function(index, field){ validateLength($(field)); });
+    if($confirm.val() != $pass.val())
+      { $confirm.addClass("error-field");
+      $pass.addClass("error-field");
+      $pass.next().text("Confirmation does not match");
+      $confirm.next().text("Password does not match"); }
+
   });
 
 });
