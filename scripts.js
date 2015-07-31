cb49bb0c0c02b12e36f8dda080baeafc51db848b
niconan <nick.hax@gmail.com>
2015-07-31 14:29:58 -0700

    add comment field char count

--- scripts.js
+++ scripts.js
@@ -2,18 +2,33 @@ $( document ).ready( function() {
 
   $("#name-form").keyup(function() {
 
-  var max = 32;
-  var currentLength = $(this).val().length;
+    var max = 32;
+    var currentLength = $(this).val().length;
 
-  if (currentLength == 0) {
-     $("#char-count").text('');
-  } else if (currentLength > max) {
-    $("#char-count").text('too many characters!');
-  } else {
-    var charsLeft = max - currentLength;
-    $("#char-count").text( charsLeft + ' characters left');
-  }
+    if (currentLength == 0) {
+       $("#char-count-name").text('');
+    } else if (currentLength > max) {
+      $("#char-count-name").text('too many characters!');
+    } else {
+      var charsLeft = max - currentLength;
+      $("#char-count-name").text( charsLeft + ' characters left');
+    }
 
-  })
+  });
 
+  $("#comment-form").keyup(function() {
+
+    var max = 140;
+    var currentLength = $(this).val().length;
+
+    if (currentLength == 0) {
+       $("#char-count-comment").text('');
+    } else if (currentLength > max) {
+      $("#char-count-comment").text('too many characters!');
+    } else {
+      var charsLeft = max - currentLength;
+      $("#char-count-comment").text( charsLeft + ' characters left');
+    }
+
+  });
 });
