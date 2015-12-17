77cc2d3780aad4f080baebbaf7ee652d87bb0436
Bideo Wego <bideowego@gmail.com>
2015-12-16 19:46:54 -0500

    Add form validation, dropdown

---
+++ dropdown.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $('.dropdown .toggle, .dropdown a').on('click', function(e) {
+      if (e.target.nodeName === 'A') {
+        var $span = $('.dropdown .toggle span');
+        var text = $(e.target).text();
+        
+        $('.dropdown .toggle')
+          .text(text)
+          .append($span);
+
+        $.post('/server.php', {"selection": text}, function(response) {
+          $('#output').text(response);
+          console.log(response);
+        });
+      }
+
+      $('.dropdown .menu').slideToggle();
+    });
+
+});
+
+
