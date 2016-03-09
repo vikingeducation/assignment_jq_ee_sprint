5b2057cd9562d19582404344904357add9f70a8c
Andrew Baik <and.baik@gmail.com>
2016-03-09 17:23:11 -0500

    finished jquery form validations

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var target = event.target;
     var characters_remaining = 32-target.value.length;
     var $username_counter = $('#username_counter');
-    
+
     if (characters_remaining === 32){
       $username_counter.hide();
     }
@@ -20,7 +20,7 @@ $(document).ready(function(){
     var target = event.target;
     var characters_remaining = 140-target.value.length;
     var $description_counter = $('#description_counter');
-    
+
     if (characters_remaining === 140){
       $description_counter.hide();
     }
@@ -37,7 +37,7 @@ $(document).ready(function(){
     var target = event.target;
     var characters_remaining = 16-target.value.length;
     var $password_counter = $('#password_counter');
-    
+
     if (characters_remaining === 16){
       $password_counter.hide();
     }
@@ -55,7 +55,7 @@ $(document).ready(function(){
     var $password_confirm_counter = $('#password_confirm_counter');
 
     var $password_value = $('#password').val();
-    
+
     if (characters_remaining === 16){
       $password_confirm_counter.hide();
     }
@@ -80,14 +80,39 @@ $(document).ready(function(){
     var $password_field = $('#password');
     var $password_confirm_field = $('#password-confirmation');
 
-    if($username_field.val().length <4 || $username_field.val().length > 32 ){
+    if ($username_field.val().length < 4){
       event.preventDefault();
       $username_field.css('color', 'red');
       $('#username_counter').css('color', 'red');
-      $username_counter.text("Your username is invalid.");
+      $('#username_counter').text("Your username needs to have at least 4 characters.");
     }
-
-    
+    if ($description_field.val().length < 4) {
+      event.preventDefault();
+      $description_field.css('color', 'red');
+      $('#description_counter').css('color', 'red');
+      $('#description_counter').text("Your description needs to have at least 4 characters.");
+    }
+    if ($password_field.val().length < 6 ) {
+      event.preventDefault();
+      $password_field.css('color', 'red');
+      $('#password_counter').css('color', 'red');
+      $('#password_counter').text("Your password needs to have at least 6 characters.");
+    }
+    // Password confirmation validation
+    if ($password_confirm_field.val().length === $password_field.val().length) {
+      for (var i = 0; i < $password_confirm_field.val().length; i++){
+        if($password_field.val()[i] != $password_confirm_field.val()[i]){
+          event.preventDefault();
+          $password_confirm_field.css('color', 'red');
+          $('#password_confirm_counter').css('color', 'red');
+          $("#password_confirm_counter").text("Password confirmation does not match.");
+        }
+      }
+    } else {
+      $password_confirm_field.css('color', 'red');
+      $('#password_confirm_counter').css('color', 'red');
+      $("#password_confirm_counter").text("Password confirmation does not match.");
+    }
 
   });
 
@@ -135,8 +160,8 @@ $(document).ready(function(){
 //     username_counter.id = 'username_counter';
 //     //$username_counter.html = characters;
 //     //console.log($username_counter);
-//     //this.after($username_counter); 
-    
+//     //this.after($username_counter);
+
 //     $(username_counter).insertAfter($target);
 
 //     }
