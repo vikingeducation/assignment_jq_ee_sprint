de2b14f557880259bde2d0e3df5f31797cce29da
Kelsey James <kelseybjames@gmail.com>
2016-03-09 10:56:25 -0800

    finished validations

--- the_script.js
+++ the_script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     if( currentText ){
       $('.remaining-characters').html( (maxTextBoxChars - currentText.length).toString() );
     } else {
-      $('.remaining-characters').html( 'some text here' );
+      $('.remaining-characters').html( '' );
     }
   });
 
@@ -19,7 +19,7 @@ $(document).ready(function() {
     if( currentText ){
       $('.remaining-characters-textarea').html( (maxTextAreaChars - currentText.length).toString() );
     } else {
-      $('.remaining-characters-textarea').html( 'some text here' );
+      $('.remaining-characters-textarea').html( '' );
     }
   });
 
@@ -29,7 +29,7 @@ $(document).ready(function() {
     if( currentText ){
       $('.remaining-characters-password').html( (maxPasswordChars - currentText.length).toString() );
     } else {
-      $('.remaining-characters-password').html( 'some text here' );
+      $('.remaining-characters-password').html( '' );
     }
   });
 
@@ -39,7 +39,7 @@ $(document).ready(function() {
     if( currentText ){
       $('.remaining-characters-confirmation').html( (maxConfirmationChars - currentText.length).toString() );
     } else {
-      $('.remaining-characters-confirmation').html( 'some text here' );
+      $('.remaining-characters-confirmation').html( '' );
     }
   });
 
@@ -53,16 +53,36 @@ $(document).ready(function() {
     }
   });
 
-  $('submit-button').on('click', function(event) {
+  $('.submit-button').click( function(event) {
+    event.preventDefault();
     var inputText = $('.valid-text').val();
     var inputTextArea = $('.valid-textarea').val();
     var passwordText = $('.valid-password').val();
     var confirmationText = $('.valid-confirmation').val();
-    event.preventDefault();
 
     if (inputText.length < 4 || inputText.length > 32) {
       $('.remaining-characters').html('Invalid length');
-      $('.valid-text').addClass('.highlight');
+      $('.valid-text').addClass('highlight');
+    }
+
+    if (inputTextArea.length < 4 || inputTextArea.length > 140) {
+      $('.remaining-characters-textarea').html('Invalid length');
+      $('.valid-textarea').addClass('highlight');
+    }
+
+    if (passwordText.length < 6 || passwordText.length > 16) {
+      $('.remaining-characters-password').html('Invalid length');
+      $('.valid-password').addClass('highlight');
+    }
+
+    if (confirmationText.length < 6 || confirmationText.length > 16) {
+      $('.remaining-characters-confirmation').html('Invalid length');
+      $('.valid-confirmation').addClass('highlight');
+    }
+
+    if (confirmationText !== passwordText) {
+      $('.password-match').html('Check passwords');
+      $('.valid-confirmation').addClass('highlight');
     }
   });
 
