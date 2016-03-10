533cd92db0b118987f0e2821440c749a3ebbbca6
Kit Langton <kitlangton@gmail.com>
2016-03-09 19:05:17 -0500

    yay

--- simon/simon.js
+++ simon/simon.js
@@ -20,69 +20,104 @@ function Simon(element) {
   this.checkGuess = function(guess) {
     var correctAnswer = this.pattern[this.position];
     if (guess == correctAnswer) {
+      this.activateButton(guess, 0, 'green');
       this.position += 1;
       if (this.position == this.pattern.length) {
-        this.position == 0;
+        this.correctAnimation();
+        this.position = 0;
         this.addToPattern();
-        this.playPattern();
+        this.updateCounter();
+        setTimeout(function() {
+          self.playPattern();
+        }, 1500);
       }
     } else {
-      console.log("wrong")
-      this.activateButton(guess);
+      this.activateButton(guess, 0, 'red');
+      this.incorrectAnimation();
+      console.log("wrong");
       this.reset();
     }
   }
 
+  this.updateCounter = function() {
+    $('#counter').html(this.pattern.length)
+  }
+
   this.buttons.click(function() {
+    console.log(self.position, self.pattern);
     var id = $(this).data('id');
     self.checkGuess(id);
   })
+
+  this.correctAnimation = function() {
+    $("#counter").animate({
+      color: 'lime'
+    }).animate({
+      color: 'black'
+    })
+  }
+
+  this.incorrectAnimation = function() {
+    $("#counter").animate({
+      color: 'red'
+    }).animate({
+      color: 'black'
+    })
+  }
 
   this.playPattern = function() {
     var time = 0;
     var active, current_button, oldColor;
     for (var j = 0; j < self.pattern.length; ++j) {
       active = self.pattern[j];
-      setTimeout(function() {self.activateButton(active)}, time);
-      time += 500;
+      self.activateButton(active, time);
+      time += 1500;
     }
   }
 
-  this.activateButton = function(active){
+  this.activateButton = function(active, time, color){
+    if (!time) {
+      time = 0;
+    }
+    if (!color) {
+      color = 'purple';
+    }
+    setTimeout(function() {
+      current_button = $(self.buttons[active]);
 
-    current_button = $(self.buttons[active]);
-    oldColor = current_button.css('borderColor');
-
-    self.catSounds[active].currentTime = 0;
-    self.catSounds[active].play();
-    current_button.animate({
-      borderColor: 'red'
-    }).animate({
-      borderColor: oldColor
-    });
+      self.catSounds[active].currentTime = 0;
+      self.catSounds[active].play();
+      current_button.animate({
+        borderColor: color
+      }).animate({
+        borderColor: '#FAA'
+      });
+    }, time);
   }
 
 
   this.reset = function() {
     this.pattern = [];
+    this.updateCounter();
   }
 
   this.start = function() {
     this.reset();
     this.addToPattern();
+    this.updateCounter();
     this.playPattern();
   }
-}
+  }
 
 
 
-$(document).ready(function() {
+  $(document).ready(function() {
 
-  var simon = new Simon('.simon')
+    var simon = new Simon('.simon')
 
-    $('.play').click(function() {
-      simon.start();
-    })
+      $('.play').click(function() {
+        simon.start();
+      })
 
 
-});
+  });
