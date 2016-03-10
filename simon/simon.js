53784a9425c7519accbee2e81277ac2571de357b
Julia Herron <julia.herron@gmail.com>
2016-03-09 16:46:38 -0800

    timer

--- simon/simon.js
+++ simon/simon.js
@@ -1,16 +1,39 @@
 function Simon(element) {
+
+  var self = this;
+
   this.buttons = $(element).find('.btn')
 
-    var self = this;
 
+  this.interval = 0;
+  this.startTimer = function() {
+    self.stopTimer();
+    self.currentTime = 5;
+    $('.timer').text(self.currentTime + " sec");
+
+    self.interval = setInterval(function() {
+      if (self.currentTime > 0) {
+      self.currentTime -= 1;
+      $('.timer').text(self.currentTime + " sec");
+      } else {
+        $('.timer').text("Time's up, you lose!");
+      }
+    }, 1000)
+  }
+
+  this.stopTimer = function() {
+    clearInterval(self.interval);
+  }
+
+  // generate random kitty
   this.pattern = [];
   this.addToPattern = function() {
     this.pattern.push(Math.floor(Math.random()*4));
   }
 
+  // create cat sounds
   this.audioFiles = ['meow.mp3','meow3.mp3','meow4.mp3','meow5.mp3','meow6.mp3']
   this.catSounds = []
-
   for (var i = 0; i < 4; ++i) {
     this.catSounds.push(new Audio(this.audioFiles[i]));
   }
@@ -30,6 +53,8 @@ function Simon(element) {
         setTimeout(function() {
           self.playPattern();
         }, 1500);
+        $('.timer').text("Correct! Watch the new pattern.")
+        self.stopTimer();
       }
     } else {
       this.activateButton(guess, 0, 'red');
@@ -43,9 +68,11 @@ function Simon(element) {
     $('#counter').html(this.pattern.length)
   }
 
+  // event listener for clicks
   this.buttons.click(function() {
     console.log(self.position, self.pattern);
     var id = $(this).data('id');
+    self.startTimer();
     self.checkGuess(id);
   })
 
@@ -73,6 +100,7 @@ function Simon(element) {
       self.activateButton(active, time);
       time += 1500;
     }
+    setTimeout(self.startTimer, time);
   }
 
   this.activateButton = function(active, time, color){
@@ -86,7 +114,9 @@ function Simon(element) {
       current_button = $(self.buttons[active]);
 
       self.catSounds[active].currentTime = 0;
+      self.catSounds[active].volume = 0.4;
       self.catSounds[active].play();
+
       current_button.animate({
         borderColor: color
       }).animate({
@@ -99,6 +129,7 @@ function Simon(element) {
   this.reset = function() {
     this.pattern = [];
     this.updateCounter();
+    $('.timer').text("Watch the pattern, click to replicate!");
   }
 
   this.start = function() {
