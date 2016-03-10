e964633ba0722c76a19867f4b468b014eb9584e3
Thomas Lo <thomasjinlo@gmail.com>
2016-03-09 23:09:50 -0800

    Photo tagger complete

--- photo.js
+++ photo.js
@@ -6,8 +6,9 @@ $(document).ready(function() {
   $tagContainer.css({"width": "100px", "height": "100px"});
   var getWidth = $tagContainer.width();
   var $photoImage = $("img");
+  var $names = $(".dtags .names");
 
-  $photoImage.mousemove(function(event){
+  $photoContainer.mousemove(function(event){
 
     var x = event.pageX;
     var y = event.pageY;
@@ -17,19 +18,63 @@ $(document).ready(function() {
       "top": y - getWidth / 2
     };
 
-    $tagContainer.css(styles);
-
+    if ($photoContainer.hasClass("searching")) {
+      $tagContainer.css(styles);
+    }
+    
   } );
 
-  $photoImage.hover(
+  $photoContainer.hover(
 
     function() {
-      console.log("hello");
       $tagContainer.show();
-
     },
     function() {
       $tagContainer.hide();
     } );
+
+
+  $photoContainer.click(function(){
+    if ($(this).hasClass("searching")){
+      $(this).removeClass("searching");
+      $tagContainer.addClass("clicked");
+    } else {
+      $(this).addClass("searching");
+      $tagContainer.removeClass("clicked");
+    }
+
+    $(".dtags").slideToggle();
+
+
+  } );
+
+
+  $names.mouseenter(function(eventObj){
+    $(this).css({
+      'background-color': 'grey'
+    } );
+  } );
+
+  $names.mouseleave(function(eventObj){
+    $(this).css({
+      'background-color': 'white'
+    } );
+  } );
+
+  $names.click(function(){
+    console.log("gets here");
+
+    var name = $(this).text();
+    var position = $tagContainer.position();
+
+    console.log(position.left);
+    square = $('<div class="tagged-box"></div>');
+    namebox = $('<div class="name-box"></div>')
+
+    $photoContainer.append(square);
+    square.css({"left": position.left, "top": position.top})
+    square.append(namebox);
+    namebox.text(name);
+  } );
 
 } );
