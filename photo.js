88838148f2e3873b001988b2ba211d8ec8b8a34e
Sia <siakaramalegos@gmail.com>
2016-05-30 19:56:20 -0500

    Hover shows tag box

--- photo.js
+++ photo.js
@@ -5,7 +5,35 @@ var PHOTO = {};
 PHOTO.PhotoModule = (function(){
 
   function init(){
+    _setHoverListener();
+  }
 
+  function _setHoverListener(){
+    $('.tag-image-container').hover(_hoverIn, _hoverOut);
+  }
+
+  function _hoverIn(event){
+    // Create the tag div
+    $taggerBox = $('<div></div>')
+      .attr('class', 'tagger-box');
+
+    // Add the tag div to the DOM
+    $('.tag-image-container').prepend($taggerBox);
+
+    // Set the mouse listener to move the tag box
+    $('#photo').mousemove(_setTagPosition);
+  }
+
+  function _hoverOut(event){
+    $('.tagger-box').remove();
+  }
+
+  function _setTagPosition(event){
+    $('.tagger-box')
+      .offset({
+        top: event.pageY - 35,
+        left: event.pageX - 35
+      });
   }
 
   return {
