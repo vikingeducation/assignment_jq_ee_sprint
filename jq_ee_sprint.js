1c4c5f6618fe35b3c51cb30c631c9de836f98133
Steven Chang <prime_pork@hotmail.com>
2016-07-03 22:49:06 +1000

    text field displays a counter and input maxes out at 32 characters in length

--- jq_ee_sprint.js
+++ jq_ee_sprint.js
@@ -1,1 +1,33 @@
 'use strict;'
+
+var controller = {
+	init: function(){
+		view.init();
+	}
+}
+
+var view = {
+	init: function(){
+		// set delegated listener on the input
+		// if input_length === 0, set display string to ""
+		// if input_length <= 32, set display string to (32 - input_length) + " characters left"
+		// if input_length >32, retrieve the first 32 characters of the value and set the value to that. Display will say "0 characters left"
+		$(":text").on( 'keyup', function(){
+			var input_length = $(event.target).val().length;
+			if (input_length === 0) {
+				$("#input-message").text("");
+			} else if (input_length < 33) {
+				var characters_left = 32 - input_length;
+				$("#input-message").text(characters_left + " characters left.");
+			} else {
+				$(event.target).val( $(event.target).val().substr(0, 32) );
+				$("#input-message").text("0 characters left.");
+			};
+		});
+	}
+}
+
+
+$(document).ready(function(){
+    controller.init();
+});
