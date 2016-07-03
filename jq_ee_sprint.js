a179b1b70ff1bc954dc169aba0c53b2604eb3b97
Steven Chang <prime_pork@hotmail.com>
2016-07-03 23:04:17 +1000

    did the same for text area and password fields

--- jq_ee_sprint.js
+++ jq_ee_sprint.js
@@ -9,21 +9,43 @@ var controller = {
 var view = {
 	init: function(){
 		// set delegated listener on the input
-		// if input_length === 0, set display string to ""
-		// if input_length <= 32, set display string to (32 - input_length) + " characters left"
-		// if input_length >32, retrieve the first 32 characters of the value and set the value to that. Display will say "0 characters left"
+		// if inputLength === 0, set display string to ""
+		// if inputLength <= 32, set display string to (32 - inputLength) + " characters left"
+		// if inputLength >32, retrieve the first 32 characters of the value and set the value to that. Display will say "0 characters left"
 		$(":text").on( 'keyup', function(){
-			var input_length = $(event.target).val().length;
-			if (input_length === 0) {
-				$("#input-message").text("");
-			} else if (input_length < 33) {
-				var characters_left = 32 - input_length;
-				$("#input-message").text(characters_left + " characters left.");
-			} else {
-				$(event.target).val( $(event.target).val().substr(0, 32) );
-				$("#input-message").text("0 characters left.");
-			};
+			var inputLength = $(event.target).val().length;
+			view.displayCharactersLeft(inputLength, event.target, "#input-message", 32);
 		});
+
+		// Doing the same for the text area.
+		$("textarea").on( 'keyup', function(){
+			var inputLength = $(event.target).val().length;
+			view.displayCharactersLeft(inputLength, event.target, "#textarea-message", 140)
+		});
+
+		// Doing the same for the password
+		$("#password").on( 'keyup', function(){
+			var inputLength = $(event.target).val().length;
+			view.displayCharactersLeft(inputLength, event.target, "#password-message", 16);
+		});
+
+		// Doing the same for the password
+		$("#confirm-password").on( 'keyup', function(){
+			var inputLength = $(event.target).val().length;
+			view.displayCharactersLeft(inputLength, event.target, "#confirm-message", 16);
+		});
+	},
+
+	displayCharactersLeft: function(inputLength, target, divId, maxCharacters){
+		if (inputLength === 0) {
+			$(divId).text("");
+		} else if (inputLength <= maxCharacters) {
+			var charactersLeft = maxCharacters - inputLength;
+			$(divId).text(charactersLeft + " characters left.");
+		} else {
+			$(target).val( $(target).val().substr(0, maxCharacters) );
+			$(divId).text("0 characters left.");
+		};
 	}
 }
 
