c5b743f64e4f6dd5d3cc9214c3ee62be31f3738d
cjvirtucio87 <cjv287@gmail.com>
2016-09-08 11:14:15 -0700

    added validation for if password confirmation is empty.

--- script.js
+++ script.js
@@ -29,14 +29,43 @@ function changeSpanValue(span,value) {
   return span.html(message);
 }
 
+function getPasswordContent() {
+  return $('#password').val();
+}
+
+function compare(value, passwordContent) {
+  return value === passwordContent;
+}
+
+function isEmpty() {
+  Array.prototype.forEach.call(arguments, function(value) {
+    if (value === '') {
+      return true;
+    }
+  });
+}
+
 $(document).ready(function(e) {
-  $("form").on("input", "input, textarea", function(e){
+  $("form").on("input", "input, textarea", function(e) {
      var charsLeft = getCharsLeft(e.target.value.length, parseInt(e.target.maxLength));
      var spanName = "#counter-"+ e.target.id;
      var $span = $(spanName);
      changeSpanValue($span, charsLeft);
-    if(e.target.value.length === 0){
+      if(e.target.value.length === 0){
+        $span.hide();
+      }
+  });
+
+  $("form").on("input", "#password-confirmation", function(e) {
+    var passwordContent = getPasswordContent();
+    var message;
+    var $span = $('#matching-pw-confirm');
+    if (compare(e.target.value, passwordContent) || isEmpty(e.target.value, passwordContent)) {
       $span.hide();
+    } else {
+      message = "Password doesn't match!";
+      changeSpanValue($span, message);
+      $span.show();
     }
   });
 });
