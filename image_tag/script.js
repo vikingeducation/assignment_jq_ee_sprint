19d329928b4791da6fa5f35c87b3fe4c4c874f3e
Dylan Lynch <lynchd2@tcnj.edu>
2016-09-08 17:39:01 -0400

    Drop down on box

--- image_tag/script.js
+++ image_tag/script.js
@@ -1,4 +1,4 @@
-var $tagBox = $('#tag-box')
+var $tagBox = $('#active')
 $("#image").on("mousemove", function(event) {
 
 
@@ -9,19 +9,25 @@ $("#image").on("mousemove", function(event) {
        top:   event.pageY - 35
     });
 
-
 })
 
 $("#image").on("mouseleave", function(event) {
   $tagBox.addClass("hidden")
 });
 
-$tagBox.on("click", function(event) {
-    $newBox = $("body").append("<div class='tag-box'></div>")
-    $tagBox.offset({
+$("#image").on("click", function(event) {
+
+    $(".container").append("<div class='tag-box'></div>")
+    $newBox = $(".tag-box").last();
+    $newBox.css({
        left:  event.pageX - 50,
        top:   event.pageY - 35
     });
-
+    $newBox.append("<ul class='box-list'><li>HI</li><li>HELLO</li><li>HOLA</li></ul>")
 })
+
+$(".box-list li").on("click", function() {
+  $(".box-list li").addClass("hidden");
+  $(this).removeClass("hidden");
+});
 
