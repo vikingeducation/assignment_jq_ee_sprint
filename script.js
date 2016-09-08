a803d8d67b04138c3ef4c07db5a9ff15161e58e0
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-09-08 09:37:58 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+});
+
+var validations = {
+  init: function () {
+    $('.form-group').each(function () {
+      this.on('keypress', charcount);
+    }
+  },
+  charcount: function () {
+    this.
+  }
+}
