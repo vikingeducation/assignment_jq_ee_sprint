7c49af315d5bd53d449676559e574dbabb9a03cd
Dylan Lynch <lynchd2@tcnj.edu>
2016-09-08 13:00:42 -0400

    Validations for jquery

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-$("#text-field").on('keydown', function() {
+var textcounter = function (selector, counter, maxCount) {
+  $(selector).on('keydown', function() {
       var letterCount = this.value.length
       if(letterCount === 0) {
-         $('#text-field-counter').text("")
+         $(counter).text("")
        } else {
-      $('#text-field-counter').text((32 - letterCount) + " characters left" )
+      $(counter).text((maxCount - letterCount) + " characters left" )
       }
     }
   )
+}
 
+textcounter("#text-field", "#text-field-counter", 32);
+textcounter("#text-area", "#text-area-counter", 140);
+textcounter("#password", "#password-counter", 16);
+textcounter("#password-conf", "#password-conf-counter", 16);
 
-$("#text-area").on('keydown', function() {
-      var letterCount = this.text().length
-      if(letterCount === 0) {
-         $('#text-area-counter').text("")
-       } else {
-      $('#text-area-counter').text((140 - letterCount) + " characters left" )
-      }
-    }
-  )
+var isValidCounter = function(selector, min, max) {
+  if !($(selector).value.length > min && $(selector).value.length < max  ) {
+    return $(selector);
+  }
+};
+
+var validPasswords = function(password, passwordconf) {
+  return $(password).value === $(passwordconf).value;
+}
+
+validPassword("#password", "#password-conf")
+
+isValidCounter("#text-field", 4, 32)
+isValidCounter("#text-area", 4, 140)
+isValidCounter("#password", 6, 16)
+isValidCounter("#password-conf", 6, 16)
+
+$('#submit').on('click', function() {
+
+});
