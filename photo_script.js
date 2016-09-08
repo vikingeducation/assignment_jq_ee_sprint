b73f79e303bbf502e84c5effc1ced55681383345
Graham Turner <turnertgraham@gmail.com>
2016-09-08 19:35:46 -0400

    double clicks

--- photo_script.js
+++ photo_script.js
@@ -1,40 +1,90 @@
 var photoMethods = {
 
+  mousemoveEvent: function(event) {
+    var div = '<div class="tagger"></div>';
+    var leftCoord = event.pageX - 50 + "px";
+    var topCoord = event.pageY - 25 + "px";
+    $('.tagger')
+      .css('top', topCoord)
+      .css('left', leftCoord);
+  },
+
   targetOutline: function() {
     $('body').append($('<div/>').addClass('tagger'));
-    $('img, .tagger').mousemove(function(event) {
-      var div = '<div class="tagger"></div>';
-      var leftCoord = event.pageX - 25 + "px";
-      var topCoord = event.pageY - 25 + "px";
-      $('.tagger')
-        .css('top', topCoord)
-        .css('left', leftCoord);
+
+    $('img').on('mousemove', photoMethods.mousemoveEvent);
+
+    $('img').hover(function() {
+      $('.tagger').toggle();
     });
-    // $('img').mouseleave(function() {
-    //   $('.tagger').hide();
-    // });
+  },
+
+  friends: ["John", "Tom", "Sarah"],
+
+  fixTargetEvent: function(event) {
+    var friendList = $("<ul class='friends'></ul>");
+    for (var i = 0; i< photoMethods.friends.length; i++) {
+      var listItem = $("<li/>").text(photoMethods.friends[i]);
+      friendList.append(listItem);
+    }
+
+    $('.tagger').hide();
+
+    $('img').off('mousemove', photoMethods.mousemoveEvent);
+
+    var leftCoord = event.pageX - 50 + "px";
+    var topCoord = event.pageY - 25 + "px";
+
+    var container = $('<div/>')
+                    .addClass('fixed-container')
+                    .css('top', topCoord)
+                    .css('left', leftCoord);
+
+    $('body').append(container);
+
+
+    container.append(
+      $('<div/>')
+      .addClass('fixed-tags')
+      );
+
+    container.append(friendList);
+
+    $('ul').click(function(e) {
+
+      var list = $(e.currentTarget);
+      list.parent().addClass('selected');
+
+      var currentEle = $(e.target);
+      currentEle.addClass("selected");
+
+      $('ul').children().filter(function(_, ele) {
+        ele = $(ele);
+        return !ele.hasClass('selected');
+      }).hide();
+      $('.tagger').hide();
+
+    });
+    $('img').on('mousemove', photoMethods.mousemoveEvent);
+    $('img').on('click', photoMethods.photoStuff);
+    $('img').off('click', photoMethods.fixTargetEvent);
+  },
+
+  photoStuff: function() {
+    if (!$('.fixed-container').last().hasClass('selected')) {
+      $('.tagger').show();
+      $('.fixed-container').last().remove();
+    }
+    $('img').on('click', photoMethods.fixTargetEvent);
+    $('img').off('click', photoMethods.photoStuff);
   },
 
   fixTarget: function() {
-    var friendList = "<ul> </ul>";
-    $('.tagger').click(function(event) {
-      var leftCoord = event.pageX - 25 + "px";
-      var topCoord = event.pageY - 25 + "px";
-      $('body').append(
-        $('<div/>')
-        .addClass('fixed-tags')
-        .css('top', topCoord)
-        .css('left', leftCoord)
-        );
-    });
+    $('img').on('click', photoMethods.fixTargetEvent);
   }
-
-
 };
 
 $(document).ready(function() {
   photoMethods.targetOutline();
   photoMethods.fixTarget();
 });
-
-
