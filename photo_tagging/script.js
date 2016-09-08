efd3c0b6789b2aa49529000ff48ba092d49fa96a
Leo Ahnn <leo.ahnn@gmail.com>
2016-09-08 17:51:10 -0400

    working on waldo

---
+++ photo_tagging/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+
+  var $img = $('img');
+  var $div = $("#tagger");
+  var $dropdown = $('<div class="dropdown"><div class="top-drop"></div><div class= "bottom-drop"><ul><li>Matt</li><li>Johnny</li><li>Leo</li></ul></div></div>')
+
+  var movingTag = function() {
+    $img.bind('mousemove', function(e) {
+      $('#tagger').css({
+        left: e.pageX - 50,
+        top: e.pageY - 50
+      });
+    });
+  }
+
+  $div.on('click', function(e) {
+    $div.addClass('fixed');
+    $img.off('mousemove');
+    var $newDrop = $dropdown.clone();
+    $(".container").append($newDrop);
+    $newDrop.css({
+      left: e.pageX - 50,
+      top: e.pageY - 50
+    });
+  });
+
+  $img.on('click', function(e) {
+    movingTag();
+    $div.removeClass('fixed');
+    $(".dropdown").remove();
+  });
+
+  $('.container').on('click', ".dropdown ul li", function(e){
+    var $parentDrop = $(this).closest('.dropdown');
+
+    $(this).parent().html($(this).clone());
+
+    $parentDrop.removeClass('dropdown').addClass('perma-drop');
+  });
+
+
+
+  movingTag();
+});
