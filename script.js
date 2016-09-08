19fe687fedf2a29c94ceb110ad2cfc8715bf9010
jsteenb2 <jonathansteenbergen@gmail.com>
2016-09-08 10:27:06 -0700

    finished up to validations

--- script.js
+++ script.js
@@ -5,13 +5,73 @@ $(document).ready(function() {
 //   if (inputLength > 32) {
 //     $feedbackDiv.text("Please input 32 characters or fewer.");
 //   };
-  var $feedbackDiv = $("#text-field-feedback");
+   var $feedbackDiv = $("#text-field-feedback");
   $('#text-field').keyup(function () {
+   
     var inputLength = $("#text-field").val().length;
     if (inputLength) {
-         $feedbackDiv.text(32 - $(this).val().length);
-    } else {
+         $feedbackDiv.text(32 - inputLength);
+    }
+    else {
       $feedbackDiv.text("");
     }
   });
+
+  $('textarea').keyup(function(){
+    var inputLength = $('textarea').val().length;
+    if (inputLength){
+      $('textarea').after($feedbackDiv.text(140 - inputLength));
+    }
+    else{
+      $feedbackDiv.text("");
+    }
+  });
+
+  $('#password').keyup(function(){
+    var inputLength = $('#password').val().length;
+    if (inputLength){
+      $('#password').after($feedbackDiv.text(16 - inputLength));
+    }
+//     else if( inputLength < 6 && inputLength){
+//       $('#password').after($feedbackDiv.text("Password to short"));
+//     }
+    else{
+      $feedbackDiv.text("");
+    }
+  });
+
+
+  var $confirmDiv = $('<div></div>');
+  $('#password-confirmation').keyup(function(){
+      var pw = $('#password').val();
+      var pwc = $('#password-confirmation').val();
+      if( pw === pwc ){
+        $('#password-confirmation').after($confirmDiv.text("Passwords match"));
+      }
+      else{
+        $('#password-confirmation').after($confirmDiv.text("Passwords don't match"));
+      }
+
+    var inputLength = $('#password-confirmation').val().length;
+    if (inputLength){
+      $('#password-confirmation').after($feedbackDiv.text(16 - inputLength));
+    }
+//     else if( inputLength < 6 && inputLength ){
+//       $('#password-confirmation').after($feedbackDiv.text("Password to short"));
+//     }
+    else{
+      $feedbackDiv.text("");
+    }
+   });
+  
+   var $errorDiv = $("<div class='error'></div>");
+   $('button').on('click', function(){
+//       $('div.error').remove();
+     
+      var textLength = $("#text-field").val().length;
+      if ( textLength < 4 || textLength > 32 ){
+        $('#text-field').addClass("error")
+        $('#text-field').after($errorDiv.clone().text("Invalid text input"));
+      }
+   })
 });
