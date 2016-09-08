733bc35420ed716639abd0b01f217f7abb320112
David Jiang <davidjgrey@gmail>
2016-09-08 10:51:04 -0700

    characters left messages complete

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 "use strict";
 
-function createSpan(charsTyped, maxChars){
-  var $span = $("<span></span>");
-  var charsLeft = maxChars - charsTyped;
-  var message = charsLeft.toString() + " characters left";
-  $span.append(message);
-  return $span;
-}
+// function createSpan(charsTyped, maxChars){
+//   var $span = $("<span></span>");
+//   var charsLeft = maxChars - charsTyped;
+//   var message = charsLeft.toString() + " characters left";
+//   $span.append(message);
+//   return $span;
+// }
 
 // function giveId(node, selector) {
 //   return node.attr('id', selector);
@@ -20,17 +20,23 @@ function createSpan(charsTyped, maxChars){
 //   return node.remove(selector);
 // }
 
-function getCharsTypedValue(value, max) {
+function getCharsLeft(value, max) {
   return max - value;
 }
 
-// function changeSpanValue(span,value) {
-//   return span.html(value.toString());
-// }
+function changeSpanValue(span,value) {
+  var message = value.toString() + " characters left";
+  return span.html(message);
+}
 
 $(document).ready(function(e) {
-  $("#text-field").on("input",function(e){
-     var charsTyped = getCharsTypedValue(e.target.value.length, 32);
-     $(this).after(span);
+  $("form").on("input", "input, textarea", function(e){
+     var charsLeft = getCharsLeft(e.target.value.length, parseInt(e.target.maxLength));
+     var spanName = "#counter-"+ e.target.id;
+     var $span = $(spanName);
+     changeSpanValue($span, charsLeft);
+    if(e.target.value.length === 0){
+      $span.hide();
+    }
   });
 });
