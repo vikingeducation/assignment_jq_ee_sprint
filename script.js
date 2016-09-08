7196c990d3f546f45148c6a934a0e2fa686951c7
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-09-08 10:25:07 -0700

    Adding password match confirmation

--- script.js
+++ script.js
@@ -4,24 +4,34 @@ $(document).ready(function () {
 
 var validations = {
   init: function () {
-    $('.form-group').each(function () {
-      $(this).on('keypress', validations.charcount);
-    });
+    // $('.form-group').each(function () {
+    //   $(this).on('keyup', validations.charcount);
+    // });
+    $('#username').on('keyup', function(e) { validations.charcount(e, 32) });
+    $('#description').on('keyup', function(e) { validations.charcount(e, 140) });
+    $('#password').on('keyup', function(e) { validations.charcount(e, 16) });
+    $('#confirm').on('keyup', function(e) { validations.charcount(e, 16) });
   },
 
   // checks value length of tag
   // creates a new tag that displays value
   // inserts new tag after textfield
-  charcount: function (event) {
-    var len = event.target.value.length + 1;
+  charcount: function (event, max) {
+    var len = max - event.target.value.length;
+    // var keyid = event.keyCode;
+    if (len !== max) {
+      if ( $(event.target).next().is("br")) {
+        var $newTag = $('<span>');
+        $newTag.insertAfter(event.target);
+      }
+      
+      $(event.target).next().text("Characters remaing: " + len);
+    } else {
+      $(event.target).next().text("");
+    }
+  },
 
-    if ( $(event.target).next().is("br")) {
-      var $newTag = $('<span>');
-      $newTag.text(len);
-      $newTag.insertAfter(event.target);
-    }
-    else {
-      $(event.target).next().text(len);
-    }
+  passwordMatch: function() {
+
   }
 };
