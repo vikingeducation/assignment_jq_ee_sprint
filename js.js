00da9cc80bdb643503e174f16acae9a258439710
David J Watts <davidjeremywatts@gmail.com>
2016-09-19 14:56:41 -0700

    some work on photo tagger

--- js.js
+++ js.js
@@ -1,6 +1,48 @@
 
 
 $(document).ready(function(){
+  ///////////////////////////
+  function getMouseCoords(e) {
+  var e = e || window.event;
+
+  }
+
+
+  var followCursor = (function() {
+    var s = document.createElement('div');
+    s.style.position = 'absolute';
+    s.style.margin = '0';
+    s.style.padding = '50px';
+    $(s).addClass("flying");
+    s.style.border = '3px solid red';
+
+    return {
+      init: function() {
+        document.body.appendChild(s);
+      },
+
+      run: function(e) {
+        var e = e || window.event;
+        s.style.left  = (e.clientX - 50) + 'px';
+        s.style.top = (e.clientY - 50) + 'px';
+        getMouseCoords(e);
+      }
+    };
+}());
+
+$("#menu ul li").hover( function() {
+  $(this).css("background-color", "blue");
+}, function() {
+  $(this).css("background-color", "green");
+});
+
+  $("img").hover( function() {
+    followCursor.init();
+    document.body.onmousemove = followCursor.run;
+  }, function () {
+    $(".flying").first().remove();
+  });
+  ////////////////////////////////
   $(".counted").on("focus",function(event){
     var count = (parseInt($(event.target).attr("maxlength")) - $(event.target).val().length).toString();
     $(event.target).val().length == 0 ? count = "" : count;
