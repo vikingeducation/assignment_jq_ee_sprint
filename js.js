473e206160204a5d3f57683a2f34534547750ce1
David Watts <davidjeremywatts@gmail.com>
2016-09-19 13:03:08 -0700

    initialize plus get through form validations

---
+++ js.js
@@ -0,0 +1,42 @@
+
+
+$(document).ready(function(){
+  $(".counted").on("focus",function(event){
+    var count = (parseInt($(event.target).attr("maxlength")) - $(event.target).val().length).toString();
+    $(event.target).val().length == 0 ? count = "" : count;
+    $("<span id='counter'>"+count +"</span>").insertAfter(event.target)
+    });
+  $(".counted").on("keyup", function(event){
+    var count = (parseInt($(event.target).attr("maxlength")) - $(event.target).val().length).toString();
+    $(event.target).val().length == 0 ? count = "" : count;
+    $("#counter").text(count);
+  });
+  $(".counted").on("blur", function(event) {
+    $("#counter").remove();
+  });
+
+  $("#confirm").on("focus",function(event){
+    if ($(event.target).val() !== $("#pass").val())
+    $("<span id='pass-match'>does not match pass</span>").insertAfter(event.target)
+    });
+  $("#confirm").on("keyup", function(event){
+    if ($(event.target).val() !== $("#pass").val()) {
+    $("#pass-match").text("does not match pass");
+    } else {
+    $("#pass-match").text("");
+    };
+
+  });
+  $("#confirm").on("blur", function(event) {
+    $("#pass-match").remove();
+  });
+
+  $("button").first().on("click", function(event){
+    if ($("textarea").first().val().length < 4 || $("#text").val().length < 4
+    || $("pass").val().length < 6 || $("confirm").val() !== $("pass").val()) {
+      event.preventDefault();
+      alert("fix your shit");
+    };
+  });
+
+})
