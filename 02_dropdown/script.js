b15079fd446159bf52bdd5d820502c6da276d14e
James Harris <computersherpa@gmail.com>
2017-01-05 15:39:16 -0600

    Fix bugs, prepare form

--- 02_dropdown/script.js
+++ 02_dropdown/script.js
@@ -1,15 +1,18 @@
 $("ul").on("click", "li", function(event){
   var $li = $(event.target);
-  if ($li.text() !== "") {
+  var $input = $('input[type=hidden]');
+  if ($('#dropdown-box span').text() !== "") {
     $("span").fadeOut(1000, function(){
       $("span").text($li.text()).fadeIn(1000);
+      $input.val($li.text());
     });
   } else {
-    $("span").text($li.text()).fadeIn(0);
-  };
+    $("span").text($li.text()).fadeIn(1000);
+    $input.val($li.text());
+  }
+  $li.parent().slideUp(2000);
 });
 
 $("#dropdown-box").on("click", function(event){
-  var $topBox = $(event.target);
-  $topBox.next().slideDown(4000);
-})
+  $('ul').slideDown(2000);
+});
