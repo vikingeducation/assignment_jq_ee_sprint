b133cb8d4c597b88e42cf38a068746516ff2facd
James Harris <computersherpa@gmail.com>
2017-01-05 17:00:06 -0600

    Prevent tagging boxes from moving outside the photo

--- 03_photo_tagging/script.js
+++ 03_photo_tagging/script.js
@@ -1,19 +1,53 @@
 var $hoverTaggingBox = jQuery('.hover-tagging-box');
+
+var calcBoxCoords = function(event) {
+  var $img = $('.photo-tagging img');
+  var imageEdges = {
+    top: $img.offset().top,
+    right: $img.offset().left + $img.width(),
+    bottom: $img.offset().top + $img.height(),
+    left: $img.offset().left
+  };
+  // check imageEdges
+  // $("<div>").width(10).height(10).css({
+  //   border: '1px solid red',
+  //   top: imageEdges.bottom,
+  //   left: imageEdges.right,
+  //   position: 'absolute'
+  // }).appendTo('body')
+
+  var x = event.pageX - 50;
+  var y = event.pageY - 50;
+  var boxSize = 106;
+
+  if (x < imageEdges.left) { x = imageEdges.left; }
+  if (x + boxSize > imageEdges.right) { x = imageEdges.right - boxSize; }
+  if (y < imageEdges.top) { y = imageEdges.top; }
+  if (y + boxSize > imageEdges.bottom) { y = imageEdges.bottom  - boxSize; }
+
+  return {
+    x: x,
+    y: y
+  };
+};
+
 $('.photo-tagging img')
   .on('mousemove', function(event) {
+    var boxCoords = calcBoxCoords(event);
     $hoverTaggingBox.css({
-      left: event.pageX - 50,
-      top: event.pageY - 50,
+      left: boxCoords.x,
+      top: boxCoords.y,
       display: "block"
     });
   }).on('mouseout', function(event) {
     $hoverTaggingBox.hide();
   }).on('click', function(event) {
+    var boxCoords = calcBoxCoords(event);
     var $placedBox = $('<div>')
       .addClass('placed-tagging-box')
       .css({
-        left: event.pageX - 50,
-        top: event.pageY - 50
-      })
-    $('.photo-tagging').append($placedBox);
+        left: boxCoords.x,
+        top: boxCoords.y,
+      });
+    $('body').append($placedBox);
   });
