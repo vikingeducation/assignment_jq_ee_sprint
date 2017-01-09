d073a252a905ce1a80b12732c0a483691e13664f
John Sawyer <sawyer@sawyermerchant.com>
2017-01-09 16:09:24 -0500

    Drop down cleaned up

--- dropdown/script.js
+++ dropdown/script.js
@@ -1,11 +1,20 @@
+var handleDrop = {
+  toggle: function() {
+    $(".options").toggleClass('show');
+  },
+
+  makePick: function() {
+    // console.log($(this).text());
+    var $selection = $(this).text();
+    var $display = $("#display"); //how do I use fewer ids?
+    $display.text($selection);
+    handleDrop.toggle($(".dropDown"));
+  }
+};
 
 $(document).ready(function() {
-  var acc = document.getElementsByClassName("dropDown");
-  var i;
-  for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-    };
-  }
+  var $dropDown = $(".dropDown");
+  var $selection = $("li");
+  $dropDown.on("click", handleDrop.toggle);
+  $selection.on("click", handleDrop.makePick);
 });
