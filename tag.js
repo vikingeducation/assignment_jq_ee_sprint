0711a36d8bf3791c9f1aa7df1d765247dedf613d
Behdad <behdad.analui@gmail.com>
2017-01-20 14:13:55 -0800

    target square moves smoothly, on click, slidedown works, hover out of image works, remaining are handle clicks on menu item and outside to cancel as well as fixing the tag on screen

--- tag.js
+++ tag.js
@@ -1,27 +1,41 @@
 var mouse_is_inside = false;
 
+
 $( document ).ready(function() {
 
-	$( ".image-container img" ).mouseover(function(e) {
-  		move_target_to(e.pageX - 50, e.pageY - 50);
+	$( "#theImageContainer" ).hover(function(e){
+		$( "#theImage" ).on('mousemove', function(e){
+			move_target(e.pageX - 50, e.pageY - 50);
+		});
+		move_target(e.pageX - 50, e.pageY - 50);
+	}, function(e){
+        $('.target').hide();
+        $('#theSelected').hide();
+        $('#theOther1').hide();
+        $('#theOther2').hide();
 	});
 
-	$( ".image-container" ).hover(function(e) {
-  			$('.target').css('display', 'block');
-		}, function(){
-			$('.target').css('display', 'none');
+	$( "#theImage" ).click(function(e){
+		$( "#theImage" ).off('mousemove');
+		$( "#theSelected" ).slideDown('fast', function(){
+			$( "#theOther1" ).slideDown('fast',function(){
+				$( "#theOther2" ).slideDown('fast');
+			});
+		});
 	});
-
-	/*$( '.target-square' ).mousedown(function(e){
-		$( ".image-container img" ).off("mouseover");
-		$( ".image-container" ).off("hover");
-		$( '.target-square' ).css('pointer-events', 'none');
-  		move_target_to(e.pageX - 50, e.pageY - 50);
-	});*/
 });
 
-function move_target_to(x, y){
+function move_target(x, y){
 	$('.target').css('display', 'block');
   	$('.target').css('top', y + "px");
   	$('.target').css('left', x + "px");
+  	var newTop = y + 100;
+  	$( "#theSelected" ).css('top', newTop + "px");
+  	$( "#theSelected" ).css('left', x + "px");
+  	newTop = newTop + 30;
+  	$( "#theOther1" ).css('top', newTop + "px");
+  	$( "#theOther1" ).css('left', x + "px");
+  	newTop = newTop + 30;
+  	$( "#theOther2" ).css('top', newTop + "px");
+  	$( "#theOther2" ).css('left', x + "px");
 }
