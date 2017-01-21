f611c8560320aa28b3e5e0a37e0eeeb137b7873f
Behdad <behdad.analui@gmail.com>
2017-01-20 20:57:12 -0800

    click on good path works

--- tag.js
+++ tag.js
@@ -1,41 +1,91 @@
-var mouse_is_inside = false;
-
+var curX = 0;
+var curY = 0;
+var slideCompleted
 
 $( document ).ready(function() {
 
 	$( "#theImageContainer" ).hover(function(e){
+		$target = create_target(e.pageX, e.pageY);
+		curX = e.pageX;
+		curY = e.pageY;
+		move_target($target, e.pageX, e.pageY);
 		$( "#theImage" ).on('mousemove', function(e){
-			move_target(e.pageX - 50, e.pageY - 50);
+			move_target($target, e.pageX, e.pageY);
 		});
-		move_target(e.pageX - 50, e.pageY - 50);
 	}, function(e){
-        $('.target').hide();
-        $('#theSelected').hide();
-        $('#theOther1').hide();
-        $('#theOther2').hide();
+			remove_target();
 	});
 
 	$( "#theImage" ).click(function(e){
-		$( "#theImage" ).off('mousemove');
-		$( "#theSelected" ).slideDown('fast', function(){
-			$( "#theOther1" ).slideDown('fast',function(){
-				$( "#theOther2" ).slideDown('fast');
+		if(e.pageY - 40 <= curX && e.pageY - 40 <= curY){
+			var targetID = '#x' + curX + 'y' + curY;
+			$(targetID).children().first().next().slideDown();
+			$( "#theImage" ).off('mousemove');
+			$( "#theImageContainer" ).off('mouseenter');
+			$( "#theImageContainer" ).off('mouseleave');
+			var $selected = $(targetID).children().first().next().children().first();
+			var $friend1 = $selected.next();
+			var $friend2 = $friend1.next();
+			var $friend3 = $friend2.next();
+			$friend1.click(function(e){
+				select_tag($selected, targetID, 'Ronaldo');
 			});
-		});
+			$friend2.click(function(e){
+				select_tag($selected, targetID, 'Rivaldo');
+			});
+			$friend3.click(function(e){
+				select_tag($selected, targetID, 'Romario');
+			});
+		}
 	});
+
 });
 
-function move_target(x, y){
-	$('.target').css('display', 'block');
-  	$('.target').css('top', y + "px");
-  	$('.target').css('left', x + "px");
-  	var newTop = y + 100;
-  	$( "#theSelected" ).css('top', newTop + "px");
-  	$( "#theSelected" ).css('left', x + "px");
-  	newTop = newTop + 30;
-  	$( "#theOther1" ).css('top', newTop + "px");
-  	$( "#theOther1" ).css('left', x + "px");
-  	newTop = newTop + 30;
-  	$( "#theOther2" ).css('top', newTop + "px");
-  	$( "#theOther2" ).css('left', x + "px");
+function select_tag(selected, targetID, selection){
+	selected.children().first().text(selection);
+	selected.css('border-bottom', '4px solid red');
+	selected.css('border-left', '4px solid red');
+	selected.css('border-right', '4px solid red');
+	selected.css('background-color', 'green');
+	selected.children().first().css('text-decoration', 'none');
+	selected.children().first().css('color', 'black');
+	$(targetID).children().first().next().slideUp();
+	$(targetID).append(selected);
+}
+
+function remove_target(){
+	$('#x' + curX + 'y' + curY).remove();
+}
+
+function move_target(target, x, y){
+	var newX = x - 40;
+	var newY = y - 40;
+	target.css('top', newY + 'px');
+	target.css('left', newX + 'px');
+}
+
+function create_target(x, y){
+	var $targetWrapper = $('<div></div>').attr('id', 'x' + x + 'y' + y).addClass('target-wrapper');
+	var $targetSquare = $('<div></div>').addClass('target-square');
+	var $friendList = $('<div></div>').addClass('friend-list');
+	var $friend1 = $('<div></div>').attr('id', 'selected').addClass('friend');
+	var $friend2 = $('<div></div>').attr('id', 'friend1').addClass('friend');
+	var $friend3 = $('<div></div>').attr('id', 'friend2').addClass('friend');
+	var $friend4 = $('<div></div>').attr('id', 'friend3').addClass('friend');
+	var $atag1 = $('<a></a>').attr('href', '#').text('');
+	var $atag2 = $('<a></a>').attr('href', '#').text('Ronaldo');
+	var $atag3 = $('<a></a>').attr('href', '#').text('Rivaldo');
+	var $atag4 = $('<a></a>').attr('href', '#').text('Romario');
+	$('.container').append($targetWrapper);
+	$targetWrapper.append($targetSquare);
+	$targetWrapper.append($friendList);
+	$friendList.append($friend1);
+	$friendList.append($friend2);
+	$friendList.append($friend3);
+	$friendList.append($friend4);
+	$friend1.append($atag1);
+	$friend2.append($atag2);
+	$friend3.append($atag3);
+	$friend4.append($atag4);
+	return $targetWrapper;
 }
