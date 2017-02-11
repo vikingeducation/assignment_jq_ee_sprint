c276e860993922613a0635fc13729bd661227cc2
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-11 23:21:32 +0000

    Add toggledDisplayedClass function

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 var hackedDropBox = {
     "init" : function init() {
         this.ulListener(this.liClicked);
+        this.ulListener(this.toggleDisplayedClass);
     },
     "ulListener" : function ulListener(fn) {
         $("ul").on("click", "li", fn);
@@ -14,6 +15,9 @@ var hackedDropBox = {
     },
     "liClicked" : function liClicked(event) {
         console.log(event.target, " was clicked");
+    },
+    "toggleDisplayedClass" : function toggleDisplayedClass(event) {
+        $(event.delegateTarget).children().toggleClass("displayed");
     }
     
 };
