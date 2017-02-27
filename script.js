dbe9efa7b0ffd4e709aff3fabe343eacb0aeaf85
Ann Allan <ann.cristina.allan@gmail.com>
2017-02-26 20:47:26 -0500

    Add counter for password and confirmation for max 16 char that disappears when text removed

--- script.js
+++ script.js
@@ -15,4 +15,20 @@ $(document).ready(function(){
         $('#emailCounter').html(140 - length);
       }
     });
+    $('#pass').keyup(function(){
+      var length = $(this).val().length;
+      if (length == 0) {
+        $('#passCounter').html('');
+      } else  {
+        $('#passCounter').html(16 - length);
+      }
+    });
+    $('#conf').keyup(function(){
+      var length = $(this).val().length;
+      if (length == 0) {
+        $('#confCounter').html('');
+      } else  {
+        $('#confCounter').html(16 - length);
+      }
+    });
 })
