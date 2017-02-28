e6f4c4e28d746c182dbee28de209eb404adcf426
Ann Allan <ann.cristina.allan@gmail.com>
2017-02-27 22:09:23 -0500

    Refactor

--- script.js
+++ script.js
@@ -1,81 +1,64 @@
-$(document).ready(function(){
-  $('#name').keyup(function(){
-    var length = $(this).val().length
-    if (length === 0) {
-      $('#nameCounter').html('');
-    } else {
-      $('#nameCounter').html(32 - length);
+$(document).ready(function () {
+  var Counter = {
+    count: function (field, fieldCounter, fieldMax) {
+      var length = field.val().length
+      if (length === 0) {
+        fieldCounter.html('');
+      } else {
+        fieldCounter.html(fieldMax - length);
+      }
     }
-  })
-  $('#email').keyup(function(){
-    var length = $(this).val().length
-    if (length === 0) {
-      $('#emailCounter').html('');
-    } else {
-      $('#emailCounter').html(140 - length);
-    }
-  })
-  $('#pass').keyup(function(){
-    var length = $(this).val().length
-    if (length === 0) {
-      $('#passCounter').html('');
-    } else {
-      $('#passCounter').html(16 - length);
-    }
-  })
-  $('#conf').keyup(function(){
-    var length = $(this).val().length
-    if (length === 0) {
-      $('#confCounter').html('');
-    } else {
-      $('#confCounter').html(16 - length);
-    }
-  })
-  $('#conf').keyup(function(){
-    var length = $(this).val().length
-    if (length === 0) {
-      $('#confMessage').html('');
-    } else if ($(this).val() != $('#pass').val()) {
-      $('#confMessage').html("Passwords do not match.");
-    } else {
-      $('#confMessage').html("")
-    }
-  })
-  $('#submit').click(function() {
-    reset()
-    var n = $('#name').val()
-    if ((n.length < 4) || (n.length > 32)) {
-      $('#nameErr').html('Name must be between 4 and 32 characters.')
-      $('#name').css('border-color', 'red')
-      return false
-    };
-    var e = $('#email').val()
-    if ((e.length < 4) || (e.length > 140)) {
-      $('#emailErr').html('Email must be between 4 and 140 characters.')
-      $('#email').css('border-color', 'red')
-      return false
-    };
-    var p = $('#pass').val()
-    if ((p.length < 6) || (p.length > 16)) {
-      $('#passErr').html('Password must be between 6 and 16 characters.')
-      $('#pass').css('border-color', 'red')
-      return false
-    };
-    var c = $('#conf').val()
-    if (c != p) {
-      $('#confErr').html('Passwords must match.')
-      $('#conf').css('border-color', 'red')
-      return false
-    };
-  })
-  var reset = function(){
-    $('#nameErr').html('')
-    $('#name').css('border-color', 'black')
-    $('#emailErr').html('')
-    $('#email').css('border-color', 'black')
-    $('#passErr').html('')
-    $('#pass').css('border-color', 'black')
-    $('#confErr').html('')
-    $('#conf').css('border-color', 'black')
   }
-})
+  var Validator = {
+    validateChars: function (field, fieldErr, fieldMin, fieldMax) {
+      if ((field.val().length < fieldMin) || (field.val().length > fieldMax)) {
+        fieldErr.html('Value must be between ' + fieldMin + ' and ' + fieldMax + ' characters.')
+        field.css('border-color', 'red')
+      };
+    },
+    validPass: function (field, pass, confErr) {
+      if (field.val() != pass.val()) {
+        confErr.html('Passwords must match.');
+        field.css('border-color', 'red');
+      };
+    },
+    confirmPass: function (field, confMes, pass) {
+      if (field.val().length === 0) {
+        confMes.html('');
+      } else if (field.val() != pass.val()) {
+        confMes.html('Passwords do not match.');
+      } else {
+        confMes.html('')
+      };
+    },
+    reset: function () {
+      $('#nameErr').html('');
+      $('#emailErr').html('');
+      $('#passErr').html('');
+      $('#confErr').html('');
+      $('input').css('border-color', 'black');
+    }
+  };
+  $('#name').keyup(function () {
+    Counter.count($('#name'), $('#nameCounter'), 32);
+  });
+  $('#email').keyup(function () {
+    Counter.count($('#email'), $('#emailCounter'), 140);
+  });
+  $('#pass').keyup(function () {
+    Counter.count($('#pass'), $('#passCounter'), 16);
+  });
+  $('#conf').keyup(function () {
+    Validator.confirmPass($('#conf'), $('#confMessage'), $('#pass'));
+    Counter.count($('#conf'), $('#confCounter'), 16);
+  });
+
+  $('#myForm').submit(function() {
+    Validator.reset();
+    Validator.validateChars($('#name'), $('#nameErr'), 4, 32);
+    Validator.validateChars($('#email'), $('#emailErr'), 4, 140);
+    Validator.validateChars($('#pass'), $('#passErr'), 6, 16);
+    Validator.validPass($('#conf'), $('#confMessage'), $('#pass'));
+    return false;
+  });
+});
