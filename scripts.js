fac7b0b445e64e244781c79ba34c18491cd771cd
Nicholas Romeo <romeonicholas@gmail.com>
2017-03-07 15:58:02 -0800

    Add logic to validation button

--- scripts.js
+++ scripts.js
@@ -3,78 +3,7 @@
 
   $(document).ready(function() {
 
-  //1. Change header
-    var changeHeader = function() {
-      var $header = $("h1");
-      $header.text("jQuerulous");
-    };
 
-    changeHeader();
-
-  //2. Create and insert an ordered list
-    var insertList = function() {
-      var $container = $(".info-box");
-
-      $container.append();
-
-    };
-
-    insertList();
-
-  //3. Change sad classes to happy
-    var sadToHappy = function() {
-      var $sadClassed = $(".sad");
-      $sadClassed.toggleClass("sad");
-      $sadClassed.toggleClass("happy");
-    };
-
-    sadToHappy();
-
-  //4. Change popup link
-
-    var linkChange = function() {
-      var $popupLink = $("#annoying-popup a");
-
-      $popupLink.attr("href", "http://www.cashcats.biz");
-    };
-
-    linkChange();
-
-  //5. Reposition popup
-
-    var movePopup = function() {
-      var $popup = $("#annoying-popup");
-
-      $popup.css("left", "75%")
-      .css("top", "+=30");
-    };
-
-    movePopup();
-
-  //6. Replace list item by traversing from another element
-
-    var replaceEllipsis = function () {
-      var $listItemsParent = $("ul");
-
-      $listItemsParent.children()
-        .each(function( index, element ) {
-          if ( element.textContent === "...") {
-            $( element ).replaceWith("<li>Additional addendums</li>");
-          }
-        });
-     };
-
-     replaceEllipsis();
-
-    //7. Replace form input with textarea
-
-    var replaceForm = function() {
-      var $inputForm = $("form input:first-child");
-
-      $inputForm.replaceWith("<textarea placeholder=\"Tell me a story!\"></textarea>");
-    };
-
-    replaceForm();
 
     // Form countdown
 
@@ -84,13 +13,17 @@
       var $inputBox = $(event.target);
       var $target = $inputBox.next();
       var currentCount = $inputBox.val().length;
-      var remainingCharacters = 32 - currentCount;
+      $inputBox.data("current", currentCount);
+
+      var maxCount = $inputBox.data("max");
+      var remainingCharacters = maxCount - currentCount;
 
       if (remainingCharacters < 0) {
         remainingCharacters = 0;
       }
 
       $target.html(remainingCharacters + " characters remaining");
+
 
       if (currentCount === 0) {
         $target.hide();
@@ -117,5 +50,31 @@
 
       });
 
-  });
+// validate field is 4-32 char, area is 4-140, pass/confirm are 6-16, passwords match
+
+      var $button = $(".button");
+
+      $button.click(function(event) {
+        event.preventDefault();
+
+        $inputs.each(function(index) {
+          var target = $(this);
+          var current = target.data("current");
+          var max = target.data("max");
+          var min = target.data("min");
+
+          if (current < min || current > max) {
+            target.addClass("error");
+            target.next(1).show();
+          }
+          else {
+            target.removeClass("error");
+            target.next(1).hide();
+          }
+          //console.log($(this).data("current"));
+        });
+      });
+
+    });
+
 } ( jQuery ) );
