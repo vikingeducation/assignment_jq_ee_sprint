ff2d88aafa87fcc9d1a8668f446dd2b4819b08ec
Christian Florez <florez.christian@gmail.com>
2017-04-10 12:38:10 -0400

    add dropdown menu

--- scripts.js
+++ scripts.js
@@ -1,63 +1,115 @@
 (function init() {
     "use strict";
-    $(document).ready(function() {
 
-        /* Structure:
-            Form Validator
-                `- Variables
-                    -input DOM elements
-                    -max length config (put this in html - data-maxlength)
-                    -
-            Dropdown Menu
-            Image Validator
+    var formValidator = {
+        init: function() {
+            formValidator.config = {
+                $formInputs: $("form *").filter(":input"),
+                $passInputs: $("form *").filter(":password"),
+                $password: $("form *")
+                    .filter(":password")
+                    .first(),
+                $passwordConfirm: $("form *")
+                    .filter(":password")
+                    .last() ,
+                $passNotification: $("p.pass-warning"),
+                $submitBtn: $("#form-submit") 
+            };
+            formValidator.setup();
+        },
 
-            --------------------
+        setup: function() {
+            formValidator.config.$formInputs.each(function(i, inputField) {
+                $(inputField).keyup(function () {
+                    var self = this;
+                    formValidator.checkInputLength($(self));
+                });
+            });
+            formValidator.config.$passwordConfirm.keyup(function () {
+                formValidator.checkPasswords(
+                    formValidator.config.$password, 
+                    formValidator.config.$passwordConfirm, 
+                    formValidator.config.$passNotification
+                );
+            });
+            formValidator.config.$submitBtn.click(function(event) {
+                event.preventDefault();
+                formValidator.validateInput(formValidator.config.$formInputs);
+            })
+        },
 
-            TODO:
-                -Remove attachListener; totally superfluous function!!!
-        */
-        var config = {maxFormLengths: [32, 140, 16, 16]};
-
-        var $formInputs = $("form *").filter(":input");
-        var $passInputs = $formInputs.filter(":password");
-        var $password = $passInputs.first();
-        var $confirmPassword = $passInputs.last(); 
-        var $passNotification = $passInputs.first().prev(".pass-warning");
-
-        var checkInputLength = function($inputField, maxLength, self) {
-            var inputLength = $(self).val().length;
-            var remaining = maxLength - inputLength;
-            var $inputNotification = $(self).next(".input-notification");
+        checkInputLength: function(self) {
+            var inputLength = self.val().length;
+            var remaining = self.attr("data-max-length") - inputLength;
+            var $inputNotification = self.next(".input-notification");
             if (inputLength) {
                 $inputNotification.text("Remaining Characters: " + remaining);
             } else {
                 $inputNotification.text("");
             }
-        };
-        var attachListener = function($inputField, maxLength) {
-            $inputField.keyup(function () {
-                var self = this;
-                checkInputLength($inputField, maxLength, self);
-            });
-        };
-        
-        $formInputs.each(function(i, inputField) {
-            attachListener($(inputField), config.maxFormLengths[i]);
-        });
+        },
 
-        $passInputs.last().keyup(function() {
-            if ($confirmPassword.val().length === 0) {
+        checkPasswords: function($password, $passwordConfirm, $passNotification) {
+            if ($passwordConfirm.val().length === 0) {
                 $passNotification.html("");
-            } else if ($password.val() !== $confirmPassword.val()) {
+            } else if ($password.val() !== $passwordConfirm.val()) {
                 $passNotification.html("Password does not match!");
             } else {
                 $passNotification.html("");
             }
-        });
+        },
 
-        $("#form-submit").click(function(event) {
-            event.preventDefault();
-            
-        });
+        validateInput: function($formInputs) {
+            $formInputs.each(function(i, inputField) {
+                var $inputField = $(inputField);
+                var $inputError = $inputField.siblings(".input-error");
+                if ($inputField.val().length > $inputField.attr("data-max-length")) {
+                    $inputField.addClass("input-error-highlight");
+                    $inputError.text("Error: Exceeded character limit for field");
+                }
+            })
+        }
+    };
+
+    var dropdown = {
+        init: function() {
+            dropdown.config = {
+                $menu: $("ul"),
+                $firstElement: $("ul li a").first()
+            };
+            dropdown.setup(dropdown.config);
+        },
+
+        setup: function(config) {
+            // Hides menu upon initial page load
+            dropdown.toggleOptions(config, 0);
+
+            config.$menu.on("click", "a", function(event) {
+                event.preventDefault();
+
+                // Compares raw DOM elements
+                var $target = $(event.target);
+                if ($target.get(0) !== config.$firstElement.get(0)) {
+                    dropdown.setTopElement($target, config.$firstElement);
+                }
+                dropdown.toggleOptions(config, 500);
+            })
+        },
+
+        // Hides and displays menu options
+        toggleOptions: function(config, speed) {
+            config.$menu.children()
+                .filter(":not(:first)")
+                .slideToggle(speed);
+        },
+
+        setTopElement: function($target, $firstElement) {
+            $firstElement.text($target.text())
+        }
+    };
+
+    $(document).ready(function() {
+        formValidator.init();
+        dropdown.init();
     });
 }());
