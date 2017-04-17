fbc03cc426094dbb406014afdf7719f04ab27041
Lorraine Chan <lchan912@gmail.com>
2017-04-16 21:58:51 -0700

    Refactor: length validations.

--- script.js
+++ script.js
@@ -1,146 +1,72 @@
+'use strict'
+
 $(document).ready(() => {
-  $('input').on('keyup', trackLength.displayAvailableLength);
-
-  $('#password_confirmation.js-password-feedback')
-    .on('keyup', password.confirmPassword);
-
-  $('.js-submit').on('click', validations.validate);
-
-  dropDown.render();
+  _registerListeners();
 });
 
-const trackLength = {
-  maxLength: {
-    name: 32,
-    summary: 140,
-    password: 16,
-    password_confirmation: 16
-  },
+const _registerListeners = () => {
+  $.each(['input', 'textarea'], (_index, tagName) => {
+    _validationListener(tagName);
+  });
+};
 
-  displayAvailableLength: event => {
-    const targetId = $(event.target)[0].id;
-    const maxLengthForTarget = trackLength.maxLength[targetId];
-    const availableLength = maxLengthForTarget - event.target.value.length
-    const containerClass = `js-${targetId}`
+const _validationListener = tagName => {
+  $('body').on('keyup', tagName, (e) => {
+    const $target = $(e.target);
+    const listeners = Object.keys(_validationListeners);
 
-    const $container = $(`.${containerClass}`)
+    let messages = [];
 
-    if(targetId) {
-      if(!$container.length) {
-        $(event.target).after($('<div/>')
-          .text(trackLength.maxLength[targetId])
-          .addClass(containerClass)
-          .css('display', 'inline-block')
-        )
+    $.each(listeners, (_index, listener) => {
+      if($target.attr(`data-${listener}`)) {
+        let results = _validationListeners[listener]($target);
+
+        if(!results.isValid) {
+          messages.push(results.message);
+        }
       }
+    })
 
-      trackLength.updateAvailableLength(
-        containerClass, availableLength, maxLengthForTarget
-      )
-    }
-  },
+    _validate($target, messages);
+  });
+};
 
-  updateAvailableLength: (containerClass, availableLength, maxLength) => {
-    const $container = $(`.${containerClass}`)
+const _validate = ($target, messages) => {
+  const errorMessageClass = `js-error-message-${$target.attr('id')}`
 
-    if(availableLength !== maxLength) {
-      $container.show().text(availableLength);
-    } else {
-      $container.hide();
-    }
+  $target.removeClass('invalid')
+  $(`.${errorMessageClass}`).remove(0);
+
+  if(messages.length) {
+    $target.addClass('invalid')
+
+    $target.after($('<span/>')
+      .addClass(errorMessageClass)
+      .text(messages.join(',')));
   }
 }
 
-const password = {
-  confirmPassword: event => {
-    const doNotMatch = 'Passwords do not match'
-    const passwordFeedbackClass = 'js-password-feedback-results'
+const _maxLength = $target => {
+  const maxLength = $target.attr('data-max-length');
+  const actualLength = $target.val().length;
 
-    const $passwordFeedback = $(`.${passwordFeedbackClass}`)
-    const $passwordField = $('#password.js-password-feedback')
-
-    const $passwordConfirmationField =
-      $('#password_confirmation.js-password-feedback')
-
-    if($passwordField.val() !== $passwordConfirmationField.val()) {
-      if(!$passwordFeedback.length) {
-        $passwordConfirmationField.after($('<div/>')
-          .text(doNotMatch)
-          .addClass(passwordFeedbackClass)
-          .css('display', 'inline-block')
-        )
-      } else {
-        $passwordFeedback.show();
-        $passwordFeedback.text(doNotMatch);
-      }
-    } else {
-      $passwordFeedback.hide();
-    }
+  return {
+    isValid: (actualLength <= maxLength || !actualLength),
+    message: `${actualLength - maxLength} over limit`
   }
 }
 
-const validations = {
-  maxLength: {
-    textField: 32,
-    textArea: 140,
-    password: 16
-  },
+const _minLength = $target => {
+  const minLength = $target.attr('data-min-length');
+  const actualLength = $target.val().length;
 
-  minLength: {
-    textField: 4,
-    textArea: 4,
-    password: 6
-  },
-
-  validate: event => {
-    event.preventDefault();
-
-    validations.validateLength(
-      $("input[type='text']"),
-      validations.maxLength.textField,
-      validations.minLength.textField
-    );
-
-
-    validations.validateLength(
-      $("input[type='textarea']"),
-      validations.maxLength.textArea,
-      validations.minLength.textArea
-    );
-
-    validations.validateLength(
-      $("input[type='password']"),
-      validations.maxLength.password,
-      validations.minLength.password
-    );
-  },
-
-  validateLength: ($field, max, min) => {
-    if ($field.val().length >= max || $field.val().length <= min) {
-      $field.addClass('errorBox');
-    } else {
-      $field.removeClass('errorBox');
-    }
+  return {
+    isValid: (actualLength >= minLength || !actualLength),
+    message: `${minLength - actualLength} characters left`
   }
 }
 
-const dropDown = {
-  render: () => {
-    const $dropDownList = $('.js-dropdown-list')
-    const $dropDownItem = $('.js-dropdown-item')
-
-    $dropDownList.children().hide();
-
-    $('<li/>').prependTo($dropDownList).show();
-
-    $dropDownList.on('click', event => {
-      $dropDownList.children().toggle(600);
-
-      $dropDownList.children().removeClass('selected');
-
-      $(event.target).addClass('selected');
-
-      $('.selected').toggle(300);
-    });
-  }
+const _validationListeners = {
+  'max-length': _maxLength,
+  'min-length': _minLength
 }
