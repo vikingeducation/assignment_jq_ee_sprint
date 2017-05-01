92879a44d43845592261c6afa3d41d3d5ecedfe8
Tyler Ketron <tketron@gmail.com>
2017-05-01 12:27:55 -0700

    refactored form validations

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
 $(document).ready(function() {
+  //form validations
+  function getLengthRemaining($target, maxLength) {
+     return (maxLength - $target.val().length);
+  }
 
-  $("#text-field").on("input", function(eventObj) {
-    var charsRemaining = 32 - $(this).val().length;
+  function updateLengthRemainingAttribute($target, maxLength) {
+    var charsRemaining = getLengthRemaining($target, maxLength);
 
-    if (charsRemaining < 32 ) {
-      $(this).next().text(charsRemaining + " characters remaining")
+    if (charsRemaining < maxLength ) {
+      $target.next().text(charsRemaining + " characters remaining")
     }
     else {
-      $(this).next().text("");
+      $target.next().text("");
     }
-  });
+  }
 
-  $("#text-area").on("input", function(eventObj) {
-    var charsRemaining = 140 - $(this).val().length;
-
-    if (charsRemaining < 140 ) {
-      $(this).next().text(charsRemaining + " characters remaining")
+  function validateLength($target, minLength, maxLength) {
+    if ($target.val().length < minLength) {
+      $target.siblings().filter(".error-message").text("Text is too short");
+    } else if ($target.val().length > maxLength) {
+      $target.siblings().filter(".error-message").text("Text is too long");
+    } else {
+      $target.siblings().filter(".error-message").text("");
     }
-    else {
-      $(this).next().text("");
-    }
-  });
+  }
 
-  $("input[type='password']").on("input", function(eventObj) {
-    var charsRemaining = 16 - $(this).val().length;
-
-    if (charsRemaining < 16 ) {
-      $(this).next().text(charsRemaining + " characters remaining")
-    }
-    else {
-      $(this).next().text("");
-    }
-
-    var password = $("#password").val();
-    var passwordConfirmation = $("#password-confirmation").val();
+  function validatePasswordMatch(password, passwordConfirmation) {
     if (password == passwordConfirmation) {
       $("#password-match-validation").text("");
     } else {
@@ -41,43 +33,64 @@ $(document).ready(function() {
         $("#password-match-validation").text("Passwords do not match!");
       }
     }
+  }
+
+  $("#text-field").on("input", function(eventObj) {
+    updateLengthRemainingAttribute($(this), 32);
+  });
+
+  $("#text-area").on("input", function(eventObj) {
+    updateLengthRemainingAttribute($(this), 140);
+  });
+
+  $("input[type='password']").on("input", function(eventObj) {
+    updateLengthRemainingAttribute($(this), 16);
+    validatePasswordMatch($("#password").val(), $("#password-confirmation").val());
+    // var password = $("#password").val();
+    // var passwordConfirmation = $("#password-confirmation").val();
+    // if (password == passwordConfirmation) {
+    //   $("#password-match-validation").text("");
+    // } else {
+    //   if (passwordConfirmation.length > 0) {
+    //     $("#password-match-validation").text("Passwords do not match!");
+    //   }
+    // }
   });
 
   $("button").on("click", function() {
     //validate text
-    if ($("#text-field").val().length < 4) {
-      $("#text-field").siblings().filter(".error-message").text("Text is too short");
-    } else if ($("#text-field").val().length > 32) {
-      $("#text-field").siblings().filter(".error-message").text("Text is too long");
-    } else {
-      $("#text-field").siblings().filter(".error-message").text("");
-    }
+    validateLength($("#text-field"), 4, 32);
+    validateLength($("#text-area"), 5, 140);
+    validateLength($("#password"), 6, 16);
+    validateLength($("#password-confirmation"), 6, 16);
 
-    //5 here since text area includes carriage return
-    if ($("#text-area").val().length < 5) {
-      $("#text-area").siblings().filter(".error-message").text("Text area is too short");
-    } else if ($("#text-area").val().length > 140) {
-      $("#text-area").siblings().filter(".error-message").text("Text area is too long");
-    } else {
-      $("#text-area").siblings().filter(".error-message").text("");
-    }
 
-    if ($("#password").val().length < 6) {
-      $("#password").siblings().filter(".error-message").text("Password is too short");
-    } else if ($("#password").val().length > 16) {
-      $("#password").siblings().filter(".error-message").text("Password is too long");
-    } else {
-      $("#password").siblings().filter(".error-message").text("");
-    }
 
-    if ($("#password-confirmation").val().length < 6) {
-      $("#password-confirmation").siblings().filter(".error-message").text("Password is too short");
-    } else if ($("#password-confirmation").val().length > 16) {
-      $("#password-confirmation").siblings().filter(".error-message").text("Password is too long");
-    } else {
-      $("#password-confirmation").siblings().filter(".error-message").text("");
-    }
+    // //5 here since text area includes carriage return
+    // if ($("#text-area").val().length < 5) {
+    //   $("#text-area").siblings().filter(".error-message").text("Text area is too short");
+    // } else if ($("#text-area").val().length > 140) {
+    //   $("#text-area").siblings().filter(".error-message").text("Text area is too long");
+    // } else {
+    //   $("#text-area").siblings().filter(".error-message").text("");
+    // }
+    //
+    // if ($("#password").val().length < 6) {
+    //   $("#password").siblings().filter(".error-message").text("Password is too short");
+    // } else if ($("#password").val().length > 16) {
+    //   $("#password").siblings().filter(".error-message").text("Password is too long");
+    // } else {
+    //   $("#password").siblings().filter(".error-message").text("");
+    // }
+    //
+    // if ($("#password-confirmation").val().length < 6) {
+    //   $("#password-confirmation").siblings().filter(".error-message").text("Password is too short");
+    // } else if ($("#password-confirmation").val().length > 16) {
+    //   $("#password-confirmation").siblings().filter(".error-message").text("Password is too long");
+    // } else {
+    //   $("#password-confirmation").siblings().filter(".error-message").text("");
   });
+
 
 
   //dropdown menu
@@ -102,6 +115,26 @@ $(document).ready(function() {
   });
 
 
+  //photo tagging
+  $("#tag-box").hide();
+  $("#tag-dropdown").hide();
+
+  $(".photo").hover(function() {
+    $("#tag-box").show();
+  }, function() {
+    $("#tag-box").hide();
+  });
+
+  $(".photo").mousemove(function(eventObj) {
+    $("#tag-box").css({
+      left: eventObj.pageX-20,
+      top: eventObj.pageY-20
+    });
+  });
+
+  $(".photo").click(function() {
+    $(".photo").off("mousemove");
+  })
 
 
 
