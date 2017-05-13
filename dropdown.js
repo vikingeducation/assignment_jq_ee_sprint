dad604ea03697982342c8fae5b6c28a95089381b
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-05-13 20:20:47 +0800

    Add dropdown

---
+++ dropdown.js
@@ -0,0 +1,24 @@
+var dropDown = {
+
+  init: function() {
+    $('.dropdown').on('click', '.toggle', dropDown.toggle);
+    $('.dropdown').on('click', '.item', dropDown.select);
+  },
+
+  toggle: function(e) {
+    var $this = $(this);
+    $this.find('.menu').slideToggle(300);
+  },
+
+  select: function(e) {
+    var $this = $(this);
+    $this.parents('.toggle').find('.selected').html($this.text());
+    dropDown.setFormValue($this);
+  },
+
+  setFormValue: function($selection) {
+    var $form = $selection.parents('.form-dropdown');
+    $form.find('#selection').val('fdjks');
+  }
+
+}
