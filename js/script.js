49cb0df678d70708dcd2fffea021640b457a3687
Luke E Summerscales <ke2uke@virginmedia.com>
2017-05-23 12:03:53 +0100

    added password correction checks

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Text limit
+
 $( document ).ready( function() {
     var max = 32;
     var left = 32;
@@ -25,4 +27,59 @@ $( document ).ready( function() {
     }
     initText();
     $("form :text").on("keyup", updateText);
+})
+
+// Text area limit
+$( document ).ready( function() {
+    var max = 140;
+    var left = 140;
+    $text = $("<p/>", {
+      html: left + " left",
+      style: "display: inline; margin-left: 10px;"
+    });
+    function initText() {
+      $( "#textarea" )
+        .parent()
+        .append($text);
+    }
+    function updateText() {
+      left = 140 - $("#textarea")
+                      .val()
+                      .length;
+      if (left <= 0)
+        $( "#textarea" )
+          .next()
+          .html("Max Length");
+      else
+        $( "#textarea" )
+          .next()
+          .html(left + " left");
+    }
+    initText();
+    $("#textarea").on("keyup", updateText);
+})
+
+$( document ).ready( function() {
+
+  function check() {
+    pass = document.getElementById("password").value;
+    confirm = document.getElementById("confirm").value;
+    if(pass == confirm) {
+      $("#password").removeClass("bad");
+      $("#confirm").removeClass("bad");
+      $("#submit").removeClass("bad");
+      $("#password").addClass("good");
+      $("#confirm").addClass("good");
+      $("#submit").addClass("good");
+    }
+    else {
+      $("#password").removeClass("good");
+      $("#confirm").removeClass("good");
+      $("#submit").removeClass("good");
+      $("#password").addClass("bad");
+      $("#confirm").addClass("bad");
+      $("#submit").addClass("bad");
+    }
+  }
+  $("#submit").on("click", check );
 })
