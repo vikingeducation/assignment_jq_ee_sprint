ae398fe6bdb6b2608c955c8963218335e11c568a
Kara Thrash <dvdonato@gmail.com>
2017-06-18 13:43:52 -0400

    test from atom

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// FILL IN THE FUNCTIONS BELOW
+ travList =  ["1st","2nd","4th","3rd"];
+
+ var CountNameCharacters = function(){
+
+ }
+
+
+var ChangeH1 =  function(){
+  document.getElementById("h1ID").innerHTML = "Cheeky";
+}
