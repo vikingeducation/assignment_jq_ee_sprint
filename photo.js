901e23a45f10351f6ac90e637a1fea92e5efe9e2
Will Timpson <will.timpson@gmail.com>
2017-06-30 13:00:50 -0400

    handlers

--- photo.js
+++ photo.js
@@ -1,3 +1,36 @@
+'use strict';
+
+var mouseHandler = {
+  init: function() {
+    $('img').on( {
+      'mousemove': mouseHandler.updateDisplay,
+      'mouseenter': mouseHandler.showHover,
+      'mouseout': mouseHandler.hideHover,
+      'click': mouseHandler.handleClick
+    } );
+  },
+
+  updateDisplay: function(event) {
+    // console.log('X: ' + event.pageX);
+    // console.log('Y: ' + event.pageY)
+  },
+
+  showHover: function(event) {
+    console.log('on image');
+  },
+
+  hideHover: function(event) {
+    console.log('off image');
+  },
+
+  handleClick: function(event) {
+    console.log('click image');
+  }
+
+}
+
 $(
-  
+  function() {
+    mouseHandler.init();
+  }
 )
