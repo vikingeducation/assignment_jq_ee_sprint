645d0dc4698ef74b90643ce325a0939e330aebd1
Will Timpson <will.timpson@gmail.com>
2017-06-29 21:43:51 -0400

    Outline label management

--- script.js
+++ script.js
@@ -4,14 +4,43 @@ var keyHandlers = {
     $('#text-area').on('keyup', keyHandlers.handleTextArea);
     $('input[type=password').on('keyup', keyHandlers.handlePassword);
   },
+
   handleText: function(event) {
-    console.log(event.target);
+    keyHandlers.checkLength($(event.target), 32);
   },
+
   handleTextArea: function(event) {
-    console.log(event.target);
+    keyHandlers.checkLength($(event.target), 140);
   },
+
   handlePassword: function(event) {
-    console.log(event.target);
+    keyHandlers.checkLength($(event.target), 16);
+  },
+
+  checkLength: function($element, maxLength) {
+    let remaining = maxLength - $element.val().length;
+
+    if (remaining === maxLength) {
+      keyHandlers.removeLabel($element);
+    } else {
+      keyHandlers.updateLabel($element, remaining);
+    }
+  },
+
+  removeLabel: function($element){
+    console.log('label removed');
+    // locate label
+    // hide label
+  },
+
+  updateLabel: function($element, remaining){
+    console.log('label updated');
+    // locate label
+    // set label value
+  },
+
+  locateLabel: function($element) {
+    return $element.next('.input-feedback');
   }
 }
 
