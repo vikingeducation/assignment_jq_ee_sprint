6e5a9949fd51eadabe0e3d3eb496b6550c929f88
Stephanie Barker <song_mage@yahoo.com>
2017-06-30 17:40:54 -0400

    made boxes on click

--- phototag/script.js
+++ phototag/script.js
@@ -4,9 +4,21 @@ $(document).ready(function() {
 		var top = e.pageY - 50;
 
 		$("#target").css({
-       left:  left,
-       top:   top,
+       left: left,
+       top: top,
     });
+	};
+
+	var targetClicked = function(e) {
+		var left = e.pageX - 50;
+		var top = e.pageY - 50;
+
+		$("<div></div>").appendTo("#container")
+			.addClass("box")
+			.css({
+				left: left,
+				top: top
+			});
 	};
 
 	$("img").on("mouseover", function(e) {
@@ -19,5 +31,9 @@ $(document).ready(function() {
 		},
 		function() {
 			$("#target").addClass("hidden");
-		});
+	});
+
+	$("#target").on("click", function(e) {
+		targetClicked(e);
+	});
 });
