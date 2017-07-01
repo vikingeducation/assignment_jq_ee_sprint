8414eaf9586cf60e66a08ad6528c209b08927d4e
Stephanie Barker <song_mage@yahoo.com>
2017-07-01 18:11:03 -0400

    finished phototag project

--- phototag/script.js
+++ phototag/script.js
@@ -1,4 +1,41 @@
 $(document).ready(function() {
+	var handlers = {
+		imgHandler: function() {
+				$("img").on("mouseover", function(e) {
+				displayTarget(e);
+			});
+		},
+
+		containerHandler: function() {
+			$("#img-container").hover(
+				function() {
+					$("#target").removeClass("hidden");
+				},
+				function() {
+					$("#target").addClass("hidden");
+			});
+
+			$("#img-container").on("click", function(e) {
+				cancelTarget(e);
+			});
+		},
+
+		targetHandler: function() {
+				$("#target").on("click", function(e) {
+				var $targetBox = makeBox(e);
+				var $nameList = assembleNames(["Grumpy", "Dopey", "Sneezy", "Doc"]);
+				targetClicked(e, $targetBox, $nameList);
+			});
+		},
+
+		liHandler: function() {
+			$("li").not(".tag").on("click", function(e) {
+				chooseTag(e, $(this));
+				e.stopPropagation();
+			});
+		}
+	};
+
 	var displayTarget = function(e) {
 		var left = e.pageX - 50;
 		var top = e.pageY - 50;
@@ -9,31 +46,82 @@ $(document).ready(function() {
     });
 	};
 
-	var targetClicked = function(e) {
+	var assembleNames = function(names) {
+		$nameList = $("<ul></ul>");
+
+		names.forEach(function(name) {
+			$nameList.append($("<li></li>")
+				.text(name)
+				.addClass("hidden"));
+		});
+
+		return $nameList
+	};
+
+	var makeBox = function(e) {
+		$targetBox = $("<div></div>")
+			.addClass("box");
+
+		return $targetBox;
+	};
+
+	var targetClicked = function(e, $targetBox, $nameList) {
+		e.stopPropagation();
 		var left = e.pageX - 50;
 		var top = e.pageY - 50;
 
-		$("<div></div>").appendTo("#container")
-			.addClass("box")
+		var $newContainer = $("<div></div>")
+			.addClass("container active")
 			.css({
+				position: "absolute",
 				left: left,
 				top: top
 			});
+
+		$("#img-container").off("mouseenter");
+
+		$newContainer.append($targetBox);
+		$newContainer.append($nameList);
+
+		$("#target").addClass("hidden");
+
+		$("#img-container").append($newContainer);
+
+		$($newContainer.find("li")).slideDown(300, function() {
+			$(this).removeClass("hidden");
+		});
+
+		handlers.liHandler();
 	};
 
-	$("img").on("mouseover", function(e) {
+	var cancelTarget = function(e, $currentTarget) {
+		$(".active").remove();
+
+		$("#target").removeClass("hidden");
+		handlers.containerHandler();
 		displayTarget(e);
-	});
+	};
 
-	$("#container").hover(
-		function() {
-			$("#target").removeClass("hidden");
-		},
-		function() {
-			$("#target").addClass("hidden");
-	});
+	var chooseTag = function(e, $element) {
+		$element.addClass("tag");
+		$element.off("click");
 
-	$("#target").on("click", function(e) {
-		targetClicked(e);
-	});
+		$(".active").removeClass("active");
+
+		$("li").not(".tag").slideUp(300, function() {
+			$(this).addClass("hidden");
+		});
+
+		$("#target").removeClass("hidden");
+		displayTarget(e);
+		handlers.containerHandler();
+	};
+
+	var addHandlers = function() {
+		handlers.imgHandler();
+		handlers.containerHandler();
+		handlers.targetHandler();
+	};
+
+	addHandlers();
 });
