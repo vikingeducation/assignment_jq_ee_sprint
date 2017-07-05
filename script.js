3e382d19b2733286c3bea16e58d7885b2d329b73
Eric Glover <etgt28@mail.missouri.edu>
2017-07-05 18:33:39 -0500

    char counter functional

--- script.js
+++ script.js
@@ -3,61 +3,60 @@
 //function to handle the letter counter
 var counterHandler = function() {
 
-  //declare update function
+
 
   //set event listener
   $(this).on("keyup", function( e ) {
-    //var $this = $(this);
     var $this = $( e.target );
       //try this with filter maybe
-    //var $counter = $this.next();
     var $counter = $( e.target ).next();
-    console.log( $counter );
-    //console.log( $(this).next());
+
+    //ADD appear on typing, hide on empty
+    if ($this.val().length > 0) {
+      //$counter.show();
+      $counter.css("display", "inline");
+    }else {
+      $counter.hide();
+    }
       //js seems to convert this for me, oddly enough (convert strings to nums to eval the expression)
-    //var remaining = $this.attr("max_characters") - $this.val().length;
     var remaining = Number( $this.attr( "max_characters" )) - $this.val().length;
-    console.log("remaining chars" + remaining);
     $counter.text(remaining);
   });
-
-
-
 }
 
 
 // execute my jquery
 $(document).ready(function() {
-  //test
-  //counterHandler.call( $("#test"), "10" );
 
-  var inputs = $("fieldset").children();
+ //ADD SOME CHARACTER COUNTERS TO THE FORM INPUTS
   var $inputs = $("input");
 
   //add max char counts
-  var $counter = $("<p></p>", { text : "32"});
+  var $counter = $("<p></p>", { text : "32"})
+    .addClass("counter");
   $counter.insertAfter($inputs.get(0));
-  $counter = $("<p></p>", { text : "140"});
+  $counter = $("<p></p>", { text : "140"})
+    .addClass("counter");
   $counter.insertAfter($inputs.get(1));
-  $counter = $("<p></p>", { text : "16"});
+  $counter = $("<p></p>", { text : "16"})
+    .addClass("counter");
   $counter.insertAfter($inputs.get(2));
-  $counter = $("<p></p>", { text : "16"});
+  $counter = $("<p></p>", { text : "16"})
+    .addClass("counter");
   $counter.insertAfter($inputs.get(3));
 
     //it seems that that my styles are applied to dom object I insert with jQuery .. hmmm...
 
 
   //add max char counts
-  $(inputs.get(0)).attr( {"max_characters" : "32" } );
-  $(inputs.get(1)).attr( {"max_characters" : "140" } );
-  $(inputs.get(2)).attr( {"max_characters" : "16" } );
-  $(inputs.get(3)).attr( {"max_characters" : "16" } );
+  $($inputs.get(0)).attr( {"max_characters" : "32" } );
+  $($inputs.get(1)).attr( {"max_characters" : "140" } );
+  $($inputs.get(2)).attr( {"max_characters" : "16" } );
+  $($inputs.get(3)).attr( {"max_characters" : "16" } );
 
   //set up listeners
-  inputs.each( function() {
-    console.log(this);
-    var max_characters = $(this).attr("max_characters");
-    counterHandler(max_characters);
-  })
-  inputs.each( counterHandler );
+  $inputs.each( counterHandler );
+
+
+
 })
