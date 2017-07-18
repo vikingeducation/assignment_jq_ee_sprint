9024640bfff01daac5982fd3df727d8bf527375e
Jeff Bernstein <jeffreybbernstein@gmail.com>
2017-07-17 21:17:33 -0700

    making progress on photo. got box to follow cursor and stamp on click

--- photo.js
+++ photo.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
-/*
-  $('img').mouseover(function(e){
-    $('#box').css({
-       left:  e.pageX,
-       top:   e.pageY
-    });
-  }); */
 
-  $('img').on('mousemove', function(e){
-    $('#box').css({
-       left:  e.pageX,
-       top:   e.pageY
-    });
+  $('img').on({
+    mouseenter: function() {
+      var $box = $('<div></div>')
+        .addClass('box');
+      $('body').append($box);
+    },
+    mouseleave: function() {
+      $(".box").remove();
+    },
+    mousemove: function(e) {
+      $(".box").css({left: e.pageX - 30, top: e.pageY - 30});
+    },
+    click: function(e) {
+      $(".box").remove();
+      var $fixedBox = $('<div></div>')
+        .addClass('fixed-box');
+      $('#img-container').append($fixedBox);
+      $fixedBox.css({left: e.pageX - 30, top: e.pageY - 30});
+    }
   });
 
 });
