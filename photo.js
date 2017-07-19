54e802139d59bffc3d08a452459c0fb4485dac81
Jeff Bernstein <jeffreybbernstein@gmail.com>
2017-07-19 12:38:44 -0700

    could only get cancel functionality to work by clicking the headline (or I could have set it to something else. need to change structure to make cancel work on clicking outside of dropdown

--- photo.js
+++ photo.js
@@ -1,40 +1,86 @@
 $(document).ready(function() {
 
+  //box that follows mouse
+  var $box = $('<div></div>')
+    .addClass('box');
+  $('#img-container').append($box);
+  $box.hide();
+
+  //box while picking name
+  var $tempBox = $('<div></div>')
+    .addClass('temp-box');
+  $('#img-container').append($tempBox);
+  $tempBox.hide();
+
+  //list of names
+  var $options = $('<ul></ul>')
+    .addClass('options')
+    .append('<li>jeff</li>')
+    .append('<li>steve</li>')
+    .append('<li>frank</li>');
+  $tempBox.after($options);
+  $options.hide();
+
+  var optionsLeft;
+  var optionsTop;
+  var tempBoxLeft;
+  var tempBoxTop;
+
   $('img').on({
     mouseenter: function() {
-      var $box = $('<div></div>')
-        .addClass('box');
-      $('#img-container').append($box);
+      $box.show();
     },
     mouseleave: function() {
-      $(".box").remove();
+      $box.hide();
     },
     mousemove: function(e) {
-      $(".box").css({left: e.pageX - 30, top: e.pageY - 30});
-    },
-    click: function(e) {
-      var $fixedBox = $('<div></div>')
-        .addClass('fixed-box');
-      $(".box").remove();
-      $('#img-container').append($fixedBox);
-      $fixedBox.css({left: e.pageX -30, top: e.pageY - 30});
-      var $options = $('<ul></ul>')
-        .addClass('options')
-        .css({left: e.pageX - 60, top: e.pageY + 30});
-      $options.append('<li>jeff</li>')
-        .append('<li>steve</li>')
-        .append('<li>frank</li>');
-      $fixedBox.after($options);
-      $options.hide();
+      $box.css({
+        left: e.pageX - 30,
+        top: e.pageY - 30
+      });
+    }
+  });
+
+  $('img').click(function(e) {
+    if ($tempBox.is(":hidden")) {
+      optionsLeft = e.pageX - 60;
+      optionsTop = e.pageY + 30;
+      tempBoxLeft = e.pageX - 30;
+      tempBoxTop = e.pageY - 30;
+      $box.hide();
+      $tempBox.show()
+        .css({
+          left: tempBoxLeft,
+          top: tempBoxTop
+        });
+      $options.css({
+        left: optionsLeft,
+        top: optionsTop
+      });
       $options.slideDown();
     }
   });
 
-  $("li").click(function() {
-    console.log('clicked');
-    var $selectedOption = $('<div></div>')
-      .addClass("selected");
+  $("#img-container").on('click', 'li', function(e) {
     var text = $(this).text();
+    var $fixedBox = $('<div></div>')
+      .addClass('fixed-box')
+      .css({
+        left: tempBoxLeft,
+        top: tempBoxTop
+      })
+      .append(text);
+    $options.hide();
+    $tempBox.hide();
+    $('#img-container').append($fixedBox);
+  });
+
+  $('body').click(function(e) {
+    if($tempBox.is(":visible") && $(e.target).is("h1")) {
+      $tempBox.hide();
+      $options.hide();
+      $box.show();
+    }
   });
 
 });
