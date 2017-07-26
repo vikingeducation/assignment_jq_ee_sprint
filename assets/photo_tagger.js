02b18c30e8d359eedb2bf14b1c0d73acf43c6f75
anne <richardson.ae@gmail.com>
2017-07-26 10:19:23 -0500

    WIP click to make box move

--- assets/photo_tagger.js
+++ assets/photo_tagger.js
@@ -2,4 +2,13 @@
 $( document ).ready(function() {
 
 
+  $( 'div.photo-container' ).on('click', 'img', function(e){
+    var boxSize = $( '.tag-box' ).css('width').slice(0, -2);
+    var newTop = (e.pageY - (boxSize / 2));
+    var newLeft = (e.pageX - (boxSize / 2));
+    $('.tag-box').css('top', newTop);
+    $('.tag-box').css('left', newLeft);
+  });
+
+
 });
