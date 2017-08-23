e7467204f5b5c420f5df2efc5863aa263331a55f
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-08-23 06:41:11 +0200

    Finishing dropdown menu

--- dropdown_script.js
+++ dropdown_script.js
@@ -1,27 +1,18 @@
 $(document).ready(function() {
 
-  $(".drop-bar").on("click", function() {
-    $(".dropdown-content").slideToggle(500);
+  $("#display").on("click", function() {
+    $("#dropdown-content").slideToggle(500);
   });
 
-  $(".option").on("click", function() {
-    console.log("clicked on option");
-    var $this = $(this);
-    $this.addClass("option-selected")
-        .removeClass("option");
-    var $replaced;
-
-    if ($(".drop-bar").length > 0) {
-      $replaced = $(".drop-bar").first();
-      $this.insertAfter($replaced);
-      $replaced.insertAfter($(".option").last()).remove();
-    } else {
-    };
-    $(".dropdown-content").slideToggle(500);
+  $("li").hover(function() {
+    $(this).addClass("option-active");
+  }, function() {
+    $(this).removeClass("option-active");
   });
 
-  $(".option-selected").on("click", function() {
-    console.log("clicked on option-selected");
-    $(".dropdown-content").slideToggle(500);
-  });
+  $("li").on("click", function() {
+    $("#display").text($(this).text());
+    $("#option-selected").val($(this).text());
+    $("#dropdown-content").slideToggle(500);
+  })
 });
