f355ef946387dc26e0fc0143d1f5f832cc099ea0
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-11-04 21:49:11 -0400

    box follows pointer

--- photo-tagging/script.js
+++ photo-tagging/script.js
@@ -4,12 +4,10 @@ $(".photo-box").mousemove(function(e){
   $("#tag-frame").remove();
   let frame='<div id="tag-frame"></div>';
   $("#container").append($(frame));
-  let yPos= e.pageY-50;
-  yPos+=""
-  setInterval(10);
-  $("#tag-frame").css("top",yPos+":px");
-  console.log("mousex: "+e.pageX);
-  console.log("mousey: "+e.pageY);
-  console.log("yPos: "+yPos);
-  console.log($("#tag-frame").css());
+  let yPos= e.pageY-80;
+  yPos+="px";
+  let xPos= e.pageX-80;
+  xPos+="px";
+  $("#tag-frame").css("top",yPos);
+  $("#tag-frame").css("left",xPos);
 })
