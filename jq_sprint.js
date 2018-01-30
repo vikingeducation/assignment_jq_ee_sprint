c67e812e3d6d39e3696a7c56c77314588626e545
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-30 09:08:49 -0500

    finished basic form for javascript character count validation, and started on JS portion

--- jq_sprint.js
+++ jq_sprint.js
@@ -1,3 +1,26 @@
+
 /*
- cd Documents/Viking/JS/jq_sprint
+  cd Documents/Viking/JS/jq_sprint
+
+  https://stackoverflow.com/questions/34453095/javascript-display-remaining-characters-of-text-area
+
+  1. get the elements themselves
+
+  2. add event listeners
+
+  3. on event trigger, calculate length
+
+  4. change the text of the element near the textbox to reflect the amount of
+  characters, these elements should be hidden otherwise, so the html should be
+  empty
 */
+
+var textInput = document.getElementById("text1").value.length;
+textInput.addEventListener("input", );
+
+function textCounter(target) {
+  console.log(textInput)
+  var characters = 32 - textInput;
+  console.log(characters)
+
+}
