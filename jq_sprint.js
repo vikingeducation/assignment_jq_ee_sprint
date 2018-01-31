753afc4c9fc37ae42a387ef3bc841dc067880f30
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-31 16:37:21 -0500

    basic method for completion, needs lots of work though

--- jq_sprint.js
+++ jq_sprint.js
@@ -2,7 +2,15 @@
 /*
   cd Documents/Viking/JS/jq_sprint
 
+  https://www.vikingcodeschool.com/dashboard#/falling-in-love-with-javascript/practice-with-events-and-effects
+
   https://stackoverflow.com/questions/34453095/javascript-display-remaining-characters-of-text-area
+
+  https://www.w3schools.com/js/js_htmldom_eventlistener.asp
+
+  https://developer.mozilla.org/en-US/docs/Web/Events/input
+
+  https://davidwalsh.name/event-delegate
 
   1. get the elements themselves
 
@@ -15,12 +23,11 @@
   empty
 */
 
-var textInput = document.getElementById("text1").value.length;
-textInput.addEventListener("input", );
-
-function textCounter(target) {
-  console.log(textInput)
-  var characters = 32 - textInput;
-  console.log(characters)
-
-}
+document.getElementById("validation").addEventListener("input", function(texty) {
+    console.log(texty.target);
+    console.log(texty.target.nodeName);
+    var characters = texty.target.value.length;
+    console.log(characters)
+    var remaining = 32 - characters;
+    console.log(remaining)
+});
