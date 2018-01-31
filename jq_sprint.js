60b6c5cef1b2c3605f4b105047d6fa34069cc0dd
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-31 17:20:13 -0500

    finished basic validation

--- jq_sprint.js
+++ jq_sprint.js
@@ -4,30 +4,29 @@
 
   https://www.vikingcodeschool.com/dashboard#/falling-in-love-with-javascript/practice-with-events-and-effects
 
-  https://stackoverflow.com/questions/34453095/javascript-display-remaining-characters-of-text-area
+  TODO
+  1. password confirmation
 
-  https://www.w3schools.com/js/js_htmldom_eventlistener.asp
-
-  https://developer.mozilla.org/en-US/docs/Web/Events/input
-
-  https://davidwalsh.name/event-delegate
-
-  1. get the elements themselves
-
-  2. add event listeners
-
-  3. on event trigger, calculate length
-
-  4. change the text of the element near the textbox to reflect the amount of
-  characters, these elements should be hidden otherwise, so the html should be
-  empty
+  2. form submission - should verify limits, password matches, and handle errors
 */
 
 document.getElementById("validation").addEventListener("input", function(texty) {
-    console.log(texty.target);
-    console.log(texty.target.nodeName);
     var characters = texty.target.value.length;
-    console.log(characters)
-    var remaining = 32 - characters;
-    console.log(remaining)
+
+    if (texty.target.classList.contains("IA")) {
+      var limit = 32;
+      var counter = document.getElementById("counterIA");
+    } else if (texty.target.classList.contains("T1")) {
+      var limit = 140;
+      var counter = document.getElementById("counterT1");
+    } else if (texty.target.classList.contains("IB")) {
+      var limit = 16;
+      var counter = document.getElementById("counterIB");
+    } else if (texty.target.classList.contains("IC")) {
+      var limit = 16;
+      var counter = document.getElementById("counterIC");
+    }
+
+    var remaining = limit - characters;
+    counter.innerHTML = "characters left: " + remaining;
 });
