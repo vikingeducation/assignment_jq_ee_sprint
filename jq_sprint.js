ff7add823a1587e4fc3961b9b165404aae138b37
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-02-01 14:28:07 -0500

    finished password confirmation

--- jq_sprint.js
+++ jq_sprint.js
@@ -1,4 +1,3 @@
-
 /*
   cd Documents/Viking/JS/jq_sprint
 
@@ -10,23 +9,42 @@
   2. form submission - should verify limits, password matches, and handle errors
 */
 
-document.getElementById("validation").addEventListener("input", function(texty) {
-    var characters = texty.target.value.length;
+document.getElementById("myForm").addEventListener("input", function(action) {
+  var subject = action.target;
+  var characters = subject.value.length;
 
-    if (texty.target.classList.contains("IA")) {
-      var limit = 32;
-      var counter = document.getElementById("counterIA");
-    } else if (texty.target.classList.contains("T1")) {
-      var limit = 140;
-      var counter = document.getElementById("counterT1");
-    } else if (texty.target.classList.contains("IB")) {
-      var limit = 16;
-      var counter = document.getElementById("counterIB");
-    } else if (texty.target.classList.contains("IC")) {
-      var limit = 16;
-      var counter = document.getElementById("counterIC");
+  if (subject.classList.contains("IA")) {
+    var limit = 32;
+    var counter = document.getElementById("counterIA");
+  } else if (subject.classList.contains("T1")) {
+    var limit = 140;
+    var counter = document.getElementById("counterT1");
+  } else if (subject.classList.contains("IB")) {
+    var limit = 16;
+    var counter = document.getElementById("counterIB");
+  } else if (subject.classList.contains("IC")) {
+    var limit = 16;
+    var counter = document.getElementById("counterIC");
+    if (
+      document.getElementsByClassName("IB")[0].value ===
+      document.getElementsByClassName("IC")[0].value
+    ) {
+      var message = "match";
+    } else {
+      var message = "don't match";
     }
+    document.getElementById("confirm").innerHTML = "passwords " + message;
+  }
 
-    var remaining = limit - characters;
-    counter.innerHTML = "characters left: " + remaining;
+  var remaining = limit - characters;
+  counter.innerHTML = "characters left: " + remaining;
 });
+
+
+
+
+
+
+
+
+// spacing
