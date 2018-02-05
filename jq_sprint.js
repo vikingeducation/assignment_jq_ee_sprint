81159a2ebd3725fed59f141f10d1c1e2c046cad5
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-02-05 15:39:58 -0500

    finished custom form error messages

--- jq_sprint.js
+++ jq_sprint.js
@@ -1,8 +1,10 @@
+
 var theForm = document.getElementById("myForm");
 
 theForm.addEventListener("input", function(action) {
   var subject = action.target;
   var characters = subject.value.length;
+  subject.classList.remove("invalid");
 
   if (subject.classList.contains("IA")) {
     var limit = 32;
@@ -19,25 +21,62 @@ theForm.addEventListener("input", function(action) {
     if (document.getElementsByClassName("IB")[0].value === subject.value) {
       var message = "match";
       document.getElementsByClassName("IB")[0].classList.remove("invalid");
-      subject.classList.remove("invalid");
       document.getElementById("confirm").classList.remove("error");
     } else {
       var message = "don't match";
     }
     document.getElementById("confirm").innerHTML = "passwords " + message;
   }
-  var remaining = limit - characters;
-  counter.innerHTML = "characters left: " + remaining;
+
+    counter.classList.remove("error");
+    var remaining = limit - characters;
+    counter.innerHTML = "characters left: " + remaining;
 });
 
 theForm.addEventListener("submit", function(pressed) {
   var inputs = document.forms[0];
+  var stop = false;
+
+  if (inputs[0].value.length < 4 || inputs[0].value.length > 32) {
+    inputs[0].classList.add("invalid");
+    var box = document.getElementById("counterIA");
+    box.innerHTML = "Error: length less that 4 characters"
+    box.classList.add("error");
+    stop = true;
+  }
+
+  if (inputs[1].value.length < 4 || inputs[1].value.length > 140) {
+    inputs[1].classList.add("invalid");
+    var box = document.getElementById("counterT1");
+    box.innerHTML = "Error: length less that 4 characters"
+    box.classList.add("error");
+    stop = true;
+  }
+
+  if (inputs[3].value.length < 6 || inputs[3].value.length > 16) {
+    inputs[3].classList.add("invalid");
+    var box = document.getElementById("counterIB");
+    box.innerHTML = "Error: length less that 6 characters"
+    box.classList.add("error");
+    stop = true;
+  }
+
+  if (inputs[4].value.length < 6 || inputs[4].value.length > 16) {
+    inputs[4].classList.add("invalid");
+    var box = document.getElementById("counterIC");
+    box.innerHTML = "Error: length less that 6 characters"
+    box.classList.add("error");
+    stop = true;
+  }
 
   if (inputs[3].value !== inputs[4].value) {
     inputs[3].classList.add("invalid");
     inputs[4].classList.add("invalid");
-    var c2 = document.getElementById("confirm");
-    c2.classList.add("error");
+    document.getElementById("confirm").classList.add("error");
+    stop = true;
+  }
+
+  if (stop === true) {
     pressed.preventDefault();
   }
 });
