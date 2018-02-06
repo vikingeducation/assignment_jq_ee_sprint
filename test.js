abfa8e41ed17921b5a0db784bf3a46fc81ee7ed3
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-02-05 23:58:43 -0500

    saving file name change

--- test.js
+++ test.js
@@ -5,6 +5,11 @@ theForm.addEventListener("input", function(action) {
   action.target.classList.remove("invalid");
   document.getElementById(action.target.getAttribute("fault")).classList.remove("error");
   document.getElementById(action.target.getAttribute("fault")).innerHTML = "characters left: " + ((action.target.getAttribute("maxy")) - (action.target.value.length));
+  if (action.target.getAttribute("twin") !== null) {
+    if (action.target.value === document.getElementsByClassName(action.target.getAttribute("twin"))[0].value) {
+      
+    }
+  }
 });
 
 theForm.addEventListener("submit", function(pressed) {
