f10114656ed48f3281158a3c7b80ad9f3ab9d658
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-02-06 00:19:26 -0500

    finished reworking password match and characters left display message rework

--- test.js
+++ test.js
@@ -2,14 +2,23 @@
 var theForm = document.getElementById("myForm");
 
 theForm.addEventListener("input", function(action) {
-  action.target.classList.remove("invalid");
-  document.getElementById(action.target.getAttribute("fault")).classList.remove("error");
-  document.getElementById(action.target.getAttribute("fault")).innerHTML = "characters left: " + ((action.target.getAttribute("maxy")) - (action.target.value.length));
-  if (action.target.getAttribute("twin") !== null) {
-    if (action.target.value === document.getElementsByClassName(action.target.getAttribute("twin"))[0].value) {
-      
+  var subject = action.target;
+  subject.classList.remove("invalid");
+  var box = document.getElementById(subject.getAttribute("fault"));
+  box.classList.remove("error");
+  box.innerHTML = "characters left: " + ((subject.getAttribute("maxy")) - (subject.value.length));
+
+  if (subject.getAttribute("twin") !== null) {
+    if (subject.value === document.getElementsByClassName(subject.getAttribute("twin"))[0].value) {
+      var message = "match";
+      document.getElementsByClassName(subject.getAttribute("twin"))[0].classList.remove("invalid");
+      document.getElementById(subject.getAttribute("verify")).classList.remove("error");
+    } else {
+      var message = "don't match";
     }
+    document.getElementById(subject.getAttribute("verify")).innerHTML = "passwords " + message;
   }
+
 });
 
 theForm.addEventListener("submit", function(pressed) {
