fce4714f9a35afb4ed5065d749f6b95e16fdd852
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-02-07 15:53:08 -0500

    building the dropdown and added jQuery

--- dropdown/dropdown.js
+++ dropdown/dropdown.js
@@ -2,4 +2,8 @@
   cd Documents/Viking/JS/jq_sprint/dropdown
 
   https://www.vikingcodeschool.com/dashboard#/falling-in-love-with-javascript/practice-with-events-and-effects
+
+  actually use jQuery for this assignment, didn't in the previous as its
+  technically just suggested and I wanted to first learn what its like to do
+  all of this without the help of a library
 */
