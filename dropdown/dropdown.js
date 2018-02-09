5518598588f8e8a32bbf485d628a648b1b6d1e72
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-02-09 15:03:32 -0500

    saving mid way through another dropdown rework, realized I haven't saved anything up to this point

--- dropdown/dropdown.js
+++ dropdown/dropdown.js
@@ -5,27 +5,11 @@
   https://www.vikingcodeschool.com/dashboard#/falling-in-love-with-javascript/practice-with-events-and-effects
 
   https://stackoverflow.com/questions/9279842/make-an-unordered-list-into-a-drop-down-menu#9280645
+
+  https://stackoverflow.com/questions/19222300/slidetoggle-and-li-repeated-clicking-reorder-the-list
 */
 
-$("#myList").hover(function() {
-  $( this).children().slideDown(2300);
-});
-
-// $( this).children().slideToggle(2300);
-
-$('li').click(function () {
-  var self = $(this),
-      siblings = self.siblings(),
-      count = siblings.length,
-      show = siblings.is(':hidden'),
-      inprogress = siblings.is(':animated');
-
-  if (inprogress) return;
-
-  siblings.slideToggle(2300, function(){
-    count--;
-    if( count === 0 && !show){
-        self.prependTo('.imp');
-    }
-  });
+$("ul").click(function(action) {
+  action.preventDefault();
+  $('ul').children().slidetoggle(1800);
 });
