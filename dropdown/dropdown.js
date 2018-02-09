b7e31acc69bac885b2c3e01fb5d4d418c70d607a
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-02-09 16:04:22 -0500

    snapshotting progress, but still having trouble getting a remnant of the list to remain after slidetoggle so there's something to re-trigger - let alone making the selected item remain

--- dropdown/dropdown.js
+++ dropdown/dropdown.js
@@ -4,15 +4,24 @@
 
   https://www.vikingcodeschool.com/dashboard#/falling-in-love-with-javascript/practice-with-events-and-effects
 
-  https://stackoverflow.com/questions/9279842/make-an-unordered-list-into-a-drop-down-menu#9280645
-
   https://stackoverflow.com/questions/19222300/slidetoggle-and-li-repeated-clicking-reorder-the-list
 */
 
 $("li").click(function(selected) {
   var list = $("ul");
-  list.children().slideToggle(1800);
+  var self = $(this);
+  var test = 0;
+  list.children().slideToggle(1800, function() {
+    test += 1
+    console.log("adding" + test)
+    if (test === 4) {
+      console.log("super broke")
+      fix()
+    }
+  });
 
-  list.prepend(this);
-
+  function fix() {
+    console.log("mega broke")
+    list.prepend(self);
+  }
 });
