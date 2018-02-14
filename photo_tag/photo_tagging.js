14aa3508f7c94d5e3b8e23c9a1e8c4db24a3ba65
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-02-14 13:30:50 -0500

    changed file paths a bit to make jQuery library file accessible to multiple project parts

--- photo_tag/photo_tagging.js
+++ photo_tag/photo_tagging.js
@@ -2,5 +2,5 @@
 /*
   cd Documents/Viking/JS/jq_sprint/photo_tag
 
-  https://www.vikingcodeschool.com/dashboard#/falling-in-love-with-javascript/practice-with-events-and-effects   
+  https://www.vikingcodeschool.com/dashboard#/falling-in-love-with-javascript/practice-with-events-and-effects
 */
