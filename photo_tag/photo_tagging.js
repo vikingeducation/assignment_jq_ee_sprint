ac0f399dc594406fda54e21f8023c64f0ff5fddc
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-02-20 23:11:09 -0500

    working through tagger menu functionality

--- photo_tag/photo_tagging.js
+++ photo_tag/photo_tagging.js
@@ -3,10 +3,6 @@
   cd Documents/Viking/JS/jq_sprint/photo_tag
 
   https://www.vikingcodeschool.com/dashboard#/falling-in-love-with-javascript/practice-with-events-and-effects
-
-  https://1stwebdesigner.com/image-tagging-tutorial/
-
-  https://stackoverflow.com/questions/22833253/simple-image-tagging-page-like-facebook-image-tag#23129551
 */
 
 $(document).ready(function() {
@@ -14,25 +10,28 @@ $(document).ready(function() {
 
   $("#pic").mousemove(function(action) {
     $(".target").css({
-      left: action.pageX - 25,
-      top: action.pageY - 25
+      top: action.pageY - 25 + "px",
+      left: action.pageX - 25 + "px"
     });
   });
 
-  $(window).mouseleave(function() {
-    $(".target").hide();
+  $(window).hover(
+    function() {
+      $(".target").show();
+    },
+    function() {
+      $(".target").hide();
+    }
+  );
+
+  $(".target").click(function(selected) {
+    $(".target")
+      .addClass("tagger")
+      .removeClass("target");
   });
-
-  $(window).mouseenter(function() {
-    $(".target").show();
-  });
-
-  /*
-
-  $("").click(function(selected) {
-
-  });
-
-  */
-
 });
+
+
+
+
+// spacing
